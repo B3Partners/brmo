1837b0e5799dfea108ab841d109175dd2fa218c6
Mark Prins <mark@b3partners.nl>
2015-02-02 14:50:05 +0100

    Gebruik JPA entities
    see #4
    
      - [x] deprecate nl.b3p.brmo.loader.entity.Bericht en  nl.b3p.brmo.loader.entity.LaadProces om de JPA equivalenten te gebruiken
      - [x] maak de STATUS in Bericht en LaadProces een enumerated value ipv String, voeg utility constructors toe
      - [x] markeer ConfigUtil als deprecated

--- brmo-persistence/src/test/java/nl/b3p/brmo/persistence/staging/LaadProcesTest.java
+++ brmo-persistence/src/test/java/nl/b3p/brmo/persistence/staging/LaadProcesTest.java
@@ -5,6 +5,9 @@ package nl.b3p.brmo.persistence.staging;
 
 import javax.persistence.Query;
 import nl.b3p.brmo.persistence.TestUtil;
+import nl.b3p.brmo.persistence.staging.LaadProces.STATUS;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
@@ -17,12 +20,16 @@ public class LaadProcesTest extends TestUtil {
     @Test
     public void shouldStoreLaadProces() {
         LaadProces p = new LaadProces();
+        p.setStatus(LaadProces.STATUS.STAGING_OK);
         entityManager.persist(p);
+        assertTrue(p.getId() > 0);
+
         Bericht b = new Bericht();
         b.setLaadprocesid(p);
         entityManager.persist(b);
         entityManager.getTransaction().commit();
 
+        assertEquals("Verwacht dezelfde status", STATUS.STAGING_OK, p.getStatus());
     }
 
     @Test
