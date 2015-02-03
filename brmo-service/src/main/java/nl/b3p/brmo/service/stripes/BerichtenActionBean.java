5750fcebb02f22ff690c0cf2083a005d6db2c44e
Mark Prins <mark@b3partners.nl>
2015-02-03 15:05:14 +0100

    BerichtenActionBean gebruikt JPA
    note de todo's in de code/commentaar
    vooralsnog zijn de methoden uit BrmoFramework getild als private members.

--- brmo-service/src/main/java/nl/b3p/brmo/service/stripes/BerichtenActionBean.java
+++ brmo-service/src/main/java/nl/b3p/brmo/service/stripes/BerichtenActionBean.java
@@ -1,8 +1,15 @@
 package nl.b3p.brmo.service.stripes;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.action.DefaultHandler;
@@ -10,14 +17,12 @@ import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.StreamingResolution;
 import net.sourceforge.stripes.validation.Validate;
-import nl.b3p.brmo.loader.BrmoFramework;
-import nl.b3p.brmo.loader.entity.Bericht;
-import nl.b3p.brmo.loader.util.BrmoException;
-import nl.b3p.brmo.service.util.ConfigUtil;
+import nl.b3p.brmo.persistence.staging.Bericht;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.stripesstuff.stripersist.Stripersist;
 
 /**
  *
@@ -54,7 +59,113 @@ public class BerichtenActionBean implements ActionBean {
         return new ForwardResolution("/WEB-INF/jsp/bericht/list.jsp");
     }
 
-    public Resolution getGridData() throws BrmoException {
+    /**
+     * @todo naar de JPA opvolger van BrmoFramework of StagingProxy overbrengen.
+     * @param sort
+     * @param dir
+     * @param filterSoort
+     * @param filterStatus
+     * @return
+     */
+    private long getCountBerichten(String sort, String dir, String filterSoort, String filterStatus) {
+        // sort en dir worden genegeerd, net als in StagingProxy..
+        final EntityManager em = Stripersist.getEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+        Root<Bericht> from = cq.from(Bericht.class);
+
+        List<Predicate> predicates = new ArrayList<Predicate>();
+        if(!filterSoort.isEmpty()) {
+            List<String> berichtSoorten = new ArrayList<String>();
+            // soort in ...
+            String[] soorten = filterSoort.split(",");
+            berichtSoorten.addAll(Arrays.asList(soorten));
+            Predicate predicate = from.get("soort").in(berichtSoorten);
+            predicates.add(predicate);
+        }
+
+        if(!filterStatus.isEmpty()) {
+            List<Bericht.STATUS> berichtStatussen = new ArrayList<Bericht.STATUS>();
+            // status in...
+            String[] statussen = filterStatus.split(",");
+            for (String s:statussen){
+                berichtStatussen.add(Bericht.STATUS.valueOf(s));
+            }
+            Predicate predicate = from.get("status").in(berichtStatussen);
+            predicates.add(predicate);
+        }
+
+        CriteriaQuery<Long> select = cq.select(cb.count(from));
+        select.where(cb.and(predicates.toArray(new Predicate[predicates.size()])));
+        return em.createQuery(select).getSingleResult();
+    }
+
+    /**
+     * @todo naar de JPA opvolger van BrmoFramework of StagingProxy overbrengen.
+     * @todo ge-dupliceerde code uit this#getCountBerichten uitfilteren
+     * @param page
+     * @param start
+     * @param limit
+     * @param sort
+     * @param dir
+     * @param filterSoort
+     * @param filterStatus
+     * @return
+     */
+    private List<Bericht> getBerichten(int page, int start, int limit, String sort, String dir, String filterSoort, String filterStatus){
+        // page word genegeerd, net als in StagingProxy#buildFilterSql
+        final EntityManager em = Stripersist.getEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Bericht> cq = cb.createQuery(Bericht.class);
+        Root<Bericht> from = cq.from(Bericht.class);
+
+        // handle filterSoort
+        List<Predicate> predicates = new ArrayList<Predicate>();
+        if(!filterSoort.isEmpty()) {
+            List<String> berichtSoorten = new ArrayList<String>();
+            // soort in ...
+            String[] soorten = filterSoort.split(",");
+            berichtSoorten.addAll(Arrays.asList(soorten));
+            Predicate predicate = from.get("soort").in(berichtSoorten);
+            predicates.add(predicate);
+        }
+        // handle filterStatus
+        if(!filterStatus.isEmpty()) {
+            List<Bericht.STATUS> berichtStatussen = new ArrayList<Bericht.STATUS>();
+            // status in...
+            String[] statussen = filterStatus.split(",");
+            for (String s:statussen){
+                berichtStatussen.add(Bericht.STATUS.valueOf(s));
+            }
+            Predicate predicate = from.get("status").in(berichtStatussen);
+            predicates.add(predicate);
+        }
+        cq.where(cb.and(predicates.toArray(new Predicate[predicates.size()])));
+
+        // handle sort & dir
+        if(sort != null && dir != null){
+            if(dir.equalsIgnoreCase("ASC")){
+                cq.orderBy(cb.asc(from.get(sort)));
+            }else{
+                cq.orderBy(cb.desc(from.get(sort)));
+            }
+
+        }
+
+
+        TypedQuery<Bericht> q = em.createQuery(cq);
+
+        if(start > 0 ){
+            // handle start  & limit
+            q.setFirstResult(start);
+        }
+        if(limit > 0){
+            q.setMaxResults(limit);
+        }
+        return q.getResultList();
+    }
+
+    public Resolution getGridData() {
 
         // ophalen filters
         String filterSoort = "";
@@ -82,23 +193,14 @@ public class BerichtenActionBean implements ActionBean {
              }
         }
 
-        DataSource dataSourceStaging = ConfigUtil.getDataSourceStaging();
         long count = 0l;
         JSONArray jsoNBerichten = new JSONArray();
-        BrmoFramework brmo = null;
-        try {
-            brmo = new BrmoFramework(dataSourceStaging, null);
 
-            count = brmo.getCountBerichten(sort, dir, filterSoort, filterStatus);
+        count = this.getCountBerichten(sort, dir, filterSoort, filterStatus);
 
-            berichten = brmo.getBerichten(page, start, limit, sort, dir, filterSoort,
+        berichten = this.getBerichten(page, start, limit, sort, dir, filterSoort,
                     filterStatus);
 
-        } finally {
-            if (brmo!=null) {
-                brmo.closeBrmoFramework();
-            }
-        }
         for (Bericht b : berichten) {
             JSONObject jsonObject = bericht2Json(b);
             jsoNBerichten.put(jsonObject);
@@ -120,11 +222,11 @@ public class BerichtenActionBean implements ActionBean {
         JSONObject json = new JSONObject();
 
         json.put("id", ber.getId());
-        json.put("object_ref", ber.getObjectRef());
+        json.put("object_ref", ber.getObject_ref());
         json.put("datum", ber.getDatum());
         json.put("soort", ber.getSoort());
         json.put("status", ber.getStatus());
-        json.put("volgordenummer", ber.getVolgordeNummer());
+        json.put("volgordenummer", ber.getVolgordenummer());
 
         return json;
     }
@@ -144,19 +246,13 @@ public class BerichtenActionBean implements ActionBean {
         };
     }
 
-    public Resolution log() throws BrmoException{
-        DataSource dataSourceStaging = ConfigUtil.getDataSourceStaging();
+    public Resolution log() {
         JSONObject jsonObj = new JSONObject();
         jsonObj.put("success", Boolean.FALSE);
-        BrmoFramework brmo = new BrmoFramework(dataSourceStaging, null);
-        try{
-            Bericht bericht = brmo.getBerichtById(selectedIds[0]);
-            jsonObj = bericht2Json(bericht);
-            jsonObj.put("opmerking", bericht.getOpmerking());
-            jsonObj.put("success", Boolean.TRUE);
-        }finally{
-            brmo.closeBrmoFramework();
-        }
+        Bericht bericht = Stripersist.getEntityManager().find(Bericht.class,selectedIds[0]);
+        jsonObj = bericht2Json(bericht);
+        jsonObj.put("opmerking", bericht.getOpmerking());
+        jsonObj.put("success", Boolean.TRUE);
         final String returnValue =  jsonObj.toString();
         return new StreamingResolution("application/json") {
             @Override
@@ -165,6 +261,8 @@ public class BerichtenActionBean implements ActionBean {
             }
         };
     }
+
+    // <editor-fold defaultstate="collapsed" desc="getters and setters">
 
     public ActionBeanContext getContext() {
         return context;
@@ -255,4 +353,6 @@ public class BerichtenActionBean implements ActionBean {
     public void setChangedItem(JSONObject changedItem) {
         this.changedItem = changedItem;
     }
+
+    //</editor-fold>
 }
