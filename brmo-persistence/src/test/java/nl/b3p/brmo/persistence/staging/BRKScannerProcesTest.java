3a3207ed57c5ca79012d14a399355da31268b243
Mark Prins <mark@b3partners.nl>
2015-02-10 12:12:00 +0100

    aanpassing van logfile naar Lob
    zodat alle loginfo per automatisch proces in de database te vinden is. Gebruik `AutomatischProces#updateSamenvattingEnLogfile(String nieuwe_samenvatting)` om de log en de samenvatting in 1x bij te werken

--- brmo-persistence/src/test/java/nl/b3p/brmo/persistence/staging/BRKScannerProcesTest.java
+++ brmo-persistence/src/test/java/nl/b3p/brmo/persistence/staging/BRKScannerProcesTest.java
@@ -30,7 +30,29 @@ public class BRKScannerProcesTest extends TestUtil {
         assertEquals("De directory is zoals geconfigureerd.", DIR, c.getScanDirectory());
         assertEquals("Verwacht dat de parameter is zoals geconfigureerd.", "true", c.getConfig().get("isActive"));
 
-        //   entityManager.remove(p);
+        entityManager.remove(c);
+        entityManager.getTransaction().commit();
+    }
+
+    @Test
+    public void testUpdateSamenvattingEnLogfile() {
+        BRKScannerProces p = new BRKScannerProces();
+        p.updateSamenvattingEnLogfile(NAAM_BESCHIJVING);
+        entityManager.persist(p);
+        final long id = p.getId();
+
+        BRKScannerProces c = entityManager.find(BRKScannerProces.class, id);
+        assertEquals("Verwacht dat de logfile en de samenvatting hetzelfde zijn.", c.getLogfile(), c.getSamenvatting());
+
+        c.updateSamenvattingEnLogfile(NAAM);
+        c.updateSamenvattingEnLogfile("2e\n" + NAAM_BESCHIJVING);
+        assertEquals(NAAM_BESCHIJVING + NAAM + "2e\n" + NAAM_BESCHIJVING, c.getLogfile());
+        entityManager.merge(c);
+
+        String[] s = p.getLogfile().split(NAAM);
+        assertEquals("Verwacht dat de logfile twee dezelfde delen bevat.", "2e\n" + s[0], s[1]);
+
+        entityManager.remove(c);
         entityManager.getTransaction().commit();
     }
 }
