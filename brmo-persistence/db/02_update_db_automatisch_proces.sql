f3efcf5a8ed48a1197464f04bd904dda17ee2a1f
Matthijs Laan <matthijsln@gmail.com>
2015-02-26 14:36:37 +0100

    script voor updaten oude install (scripts voor nu alleen postgresql)

---
+++ brmo-persistence/db/02_update_db_automatisch_proces.sql
@@ -0,0 +1,22 @@
+    create table automatisch_proces (
+        dtype varchar(255) not null,
+        id  bigserial not null,
+        lastrun timestamp,
+        logfile text,
+        samenvatting text,
+        status varchar(255),
+        primary key (id)
+    );
+
+    create table automatisch_proces_config (
+        proces_id int8 not null,
+        config text,
+        config_key varchar(255),
+        primary key (proces_id, config_key)
+    );
+
+
+    alter table automatisch_proces_config 
+        add constraint FK39F3573E561B9F9B 
+        foreign key (proces_id) 
+        references automatisch_proces;
