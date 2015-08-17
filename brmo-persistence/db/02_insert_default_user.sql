48ceca054df4c64eeb508e31668d7cff36696d28
cvanlith <chrisvanlith@gmail.com>
2015-08-17 15:01:17 +0200

    toegevoegd insert script voor admin user

---
+++ brmo-persistence/db/02_insert_default_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO groep_ VALUES ('Admin', 'Groep met toegang tot BRMO service');
+INSERT INTO gebruiker_ VALUES ('brmo', '6310227872580fec7d1262ab7ab3b4b3902a9f61');
+INSERT INTO gebruiker_groepen VALUES ('brmo', 'Admin');
+
