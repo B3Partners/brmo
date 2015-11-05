19f993f33268228f901a4a23655310056c5d491d
Mark Prins <mark@b3partners.nl>
2015-11-05 12:32:08 +0100

    voeg een utility script toe Om en geotools / geoserver primary key tabel aan te maken en (deels) te vullen voor de views.

---
+++ datamodel/utility_scripts/postgresql/403_create_geotools_primarykey_metatable.sql
@@ -0,0 +1,44 @@
+--
+-- Maak een Geotools/Geoserver primary key metatabel aan, de metatabel 
+-- kan vervolgens gebruikt worden voor het invulveld "Primary key metadata table" 
+-- in de Geoserver bron configuratie. Uit te voeren als schema eigenaar.
+--
+-- zie: http://docs.geoserver.org/stable/en/user/data/database/primarykey.html
+--
+-- NB. let op de schema naam 'brmo_rsgb' in dit bestand; die dient vervangen te worden door de juiste naam.
+-- NB. de lijst views is mogelijk niet compleet, afhankelijk van installatie
+-- 
+CREATE TABLE gt_pk_metadata (
+    table_schema VARCHAR(32) NOT NULL,
+    table_name VARCHAR(32) NOT NULL,
+    pk_column VARCHAR(32) NOT NULL,
+    pk_column_idx INTEGER,
+    pk_policy VARCHAR(32),
+    pk_sequence VARCHAR(64),
+    unique (table_schema, table_name, pk_column),
+    check (pk_policy in ('sequence', 'assigned', 'autoincrement'))
+);
+
+CREATE UNIQUE INDEX gt_pk_metadata_idx 
+  ON gt_pk_metadata (table_schema, table_name, pk_column);
+
+--
+-- insert de primary key metadata voor de views
+--
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_adres', 'fid', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_adres_ligplaats', 'fid', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_adres_standplaats', 'fid', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_adres_totaal', 'fid', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_bd_app_re_bij_perceel', 'sc_kad_identif', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_bd_kad_perceel_met_app', 'sc_kad_identif', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_kad_perceel_eenvoudig', 'sc_kad_identif', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_kad_perceel_in_eigendom', 'sc_kad_identif', NULL, 'assigned', NULL);
+-- in V_KAD_PERCEEL_ZR_ADRESSEN is SC_KAD_IDENTIF niet uniek, derhalve is er geen bruikbaar primary key veld!
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_ligplaats', 'sc_kad_identif', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_map_kad_perceel', 'sc_kad_identif', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_pand_gebruik_niet_ingemeten', 'fid', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_pand_in_gebruik', 'fid', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_standplaats', 'sc_identif', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_verblijfsobject', 'fid', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_verblijfsobject_alles', 'fid', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('brmo_rsgb', 'v_verblijfsobject_gevormd', 'fid', NULL, 'assigned', NULL);
