bccc1841dab9d45bb01de4879649a3f7f740541a
NiekBouma <niek.bouma19@gmail.com>
2016-02-17 17:12:30 +0100

    Update v_kad_perceel_metgeeom.sql

--- datamodel/utility_scripts/unsupported/flevoland/v_kad_perceel_metgeeom.sql
+++ datamodel/utility_scripts/unsupported/flevoland/v_kad_perceel_metgeeom.sql
@@ -3,4 +3,34 @@
 --Datum: 17/02/2016
 --Auteur: Meine Toonen
 
-select * from kad_perceel p where p.begrenzing_perceel.sdo_srid != 0 
+CREATE VIEW
+    V_KAD_PERCEEL_METGEOM
+    (
+        SC_KAD_IDENTIF,
+        AAND_SOORT_GROOTTE,
+        GROOTTE_PERCEEL,
+        OMSCHR_DEELPERCEEL,
+        FK_7KDP_SC_KAD_IDENTIF,
+        KA_DEELPERCEELNUMMER,
+        KA_KAD_GEMEENTECODE,
+        KA_PERCEELNUMMER,
+        KA_SECTIE,
+        BEGRENZING_PERCEEL,
+        PLAATSCOORDINATEN_PERCEEL
+    ) AS
+SELECT
+    "SC_KAD_IDENTIF",
+    "AAND_SOORT_GROOTTE",
+    "GROOTTE_PERCEEL",
+    "OMSCHR_DEELPERCEEL",
+    "FK_7KDP_SC_KAD_IDENTIF",
+    "KA_DEELPERCEELNUMMER",
+    "KA_KAD_GEMEENTECODE",
+    "KA_PERCEELNUMMER",
+    "KA_SECTIE",
+    "BEGRENZING_PERCEEL",
+    "PLAATSCOORDINATEN_PERCEEL"
+FROM
+    kad_perceel p
+WHERE
+    p.begrenzing_perceel.sdo_srid != 0;
