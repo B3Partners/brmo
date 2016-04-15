8d5a7abae9259518cbebd0bcc2c70a2057b89e0d
Matthijs Laan <matthijsln@gmail.com>
2016-04-15 17:34:48 +0200

    Update met 115_nhr.sql extra script erbij

--- datamodel/generated_scripts/datamodel_postgresql.sql
+++ datamodel/generated_scripts/datamodel_postgresql.sql
@@ -19836,3 +19836,19 @@ alter table zak_recht drop constraint fk_zkr_rl_3;
 -- Rechterkant mogelijk nog niet geinsert
 alter table kad_onrrnd_zk_his_rel drop constraint fk_kad_onrrnd_zk_his_rel_sc_rh;
 
+-- Script: 115_nhr.sql
+
+
+
+
+alter table sbi_activiteit alter column omschr type character varying(255);
+
+create table vestg_activiteit(
+    fk_vestg_nummer varchar(32) references vestg(sc_identif),
+    fk_sbi_activiteit_code varchar(6) references sbi_activiteit(sbi_code),
+    indicatie_hoofdactiviteit numeric(1,0),
+    primary key(fk_vestg_nummer, fk_sbi_activiteit_code)
+);
+
+ALTER TABLE vestg_naam
+  ADD PRIMARY KEY (naam, fk_ves_sc_identif);
