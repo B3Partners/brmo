5ffac24f559494df2edcd3b8feb6729ed81aeb4c
cvanlith <chrisvanlith@gmail.com>
2016-05-03 14:19:59 +0200

    eigenarenkaart view toegevoegd

--- datamodel/extra_scripts/postgresql/107_brk_views.sql
+++ datamodel/extra_scripts/postgresql/107_brk_views.sql
@@ -186,4 +186,144 @@ where bd1.omschrijving like 'betrokkenBij%'
 and zr2.FK_8PES_SC_IDENTIF is not null
 order by kpe.SC_KAD_IDENTIF, kpe.straat, kpe.huisnummer, kpe.toevoeging, kpe.huisletter,  KA_APPARTEMENTSINDEX::int;
 
+-- percelen plus appartementen op de percelen
+CREATE OR REPLACE VIEW v_bd_app_re_and_kad_perceel AS
+select
+    p.sc_kad_identif    AS kadaster_identificatie,
+    'perceel' as type,
+    p.ka_deelperceelnummer,
+    '' as ka_appartementsindex,
+    p.ka_perceelnummer,
+    p.ka_kad_gemeentecode,
+    p.ka_sectie,
+    p.begrenzing_perceel
+FROM
+    kad_perceel p
+union   
+SELECT 
+    ar.sc_kad_identif,
+    'appartement' as type,
+    '' as ka_deelperceelnummer,
+    ar.ka_appartementsindex,
+    ar.ka_perceelnummer,
+    ar.ka_kad_gemeentecode,
+    ar.ka_sectie,
+    kp.begrenzing_perceel
+   FROM v_bd_app_re_all_kad_perceel v
+     JOIN kad_perceel kp ON v.perceel_identif::NUMERIC = kp.sc_kad_identif
+     JOIN app_re ar ON v.app_re_identif::NUMERIC = ar.sc_kad_identif;    
+
+-- Eigenarenkaart - percelen en appartementen met hun eigenaren
+CREATE OR REPLACE VIEW
+    V_KAD_EIGENARENKAART
+    (
+        OBJECTID,
+        KADASTER_IDENTIFICATIE,
+        TYPE,
+        ZAKELIJK_RECHT_IDENTIFICATIE,
+        AANDEEL_TELLER,
+        AANDEEL_NOEMER,
+        AARD_RECHT_AAND,
+        ZAKELIJK_RECHT_OMSCHRIJVING,
+        SOORT_EIGENAAR,
+        GESLACHTSNAAM,
+        VOORVOEGSEL,
+        VOORNAMEN,
+        GESLACHT,
+        PERCEEL_ZAK_RECHT_NAAM,
+        PERSOON_IDENTIFICATIE,
+        WOONADRES,
+        GEBOORTEDATUM,
+        GEBOORTEPLAATS,
+        OVERLIJDENSDATUM,
+        NAAM_NIET_NATUURLIJK_PERSOON,
+        RECHTSVORM,
+        STATUTAIRE_ZETEL,
+        KVK_NUMMER,
+        KA_APPARTEMENTSINDEX,
+        KA_DEELPERCEELNUMMER,
+        KA_PERCEELNUMMER,
+        KA_KAD_GEMEENTECODE,
+        KA_SECTIE,
+        BEGRENZING_PERCEEL
+    ) AS
+SELECT
+    row_number() OVER () AS objectid,
+    p.kadaster_identificatie    AS kadaster_identificatie,
+    p.type,
+    zr.kadaster_identif AS zakelijk_recht_identificatie,
+    zr.ar_teller        AS aandeel_teller,
+    zr.ar_noemer        AS aandeel_noemer,
+    zr.fk_3avr_aand     AS aard_recht_aand,
+    ark.omschr          AS zakelijk_recht_omschrijving,
+    CASE
+        WHEN np.sc_identif IS NOT NULL
+        THEN 'Natuurlijk persoon'
+        WHEN nnp.sc_identif IS NOT NULL
+        THEN 'Niet natuurlijk persoon'
+        ELSE 'Onbekend'
+    END                             AS soort_eigenaar,
+    np.nm_geslachtsnaam             AS geslachtsnaam,
+    np.nm_voorvoegsel_geslachtsnaam AS voorvoegsel,
+    np.nm_voornamen                 AS voornamen,
+    np.geslachtsaand                AS geslacht,
+    CASE
+        WHEN np.sc_identif IS NOT NULL
+        THEN np.NM_GESLACHTSNAAM || ', ' || np.NM_VOORNAMEN || ' ' ||
+            np.NM_VOORVOEGSEL_GESLACHTSNAAM
+        WHEN nnp.sc_identif IS NOT NULL
+        THEN nnp.NAAM
+        ELSE 'Onbekend'
+    END                     AS perceel_zak_recht_naam,
+    inp.sc_identif          AS persoon_identificatie,
+    inp.va_loc_beschrijving AS woonadres,
+    inp.gb_geboortedatum    AS geboortedatum,
+    inp.gb_geboorteplaats   AS geboorteplaats,
+    inp.ol_overlijdensdatum AS overlijdensdatum,
+    nnp.naam                AS naam_niet_natuurlijk_persoon,
+    innp.rechtsvorm,
+    innp.statutaire_zetel,
+    innp_subject.kvk_nummer,
+    p.ka_appartementsindex,
+    p.ka_deelperceelnummer,
+    p.ka_perceelnummer,
+    p.ka_kad_gemeentecode,
+    p.ka_sectie,
+    p.begrenzing_perceel
+FROM
+    v_bd_app_re_and_kad_perceel p
+JOIN
+    zak_recht zr
+ON
+    zr.fk_7koz_kad_identif = p.kadaster_identificatie
+LEFT JOIN
+    aard_recht_verkort ark
+ON
+    zr.fk_3avr_aand = ark.aand
+LEFT JOIN
+    aard_verkregen_recht ar
+ON
+    zr.fk_3avr_aand = ar.aand
+LEFT JOIN
+    nat_prs np
+ON
+    np.sc_identif = zr.fk_8pes_sc_identif
+LEFT JOIN
+    ingeschr_nat_prs inp
+ON
+    inp.sc_identif = np.sc_identif
+LEFT JOIN
+    niet_nat_prs nnp
+ON
+    nnp.sc_identif = zr.fk_8pes_sc_identif
+LEFT JOIN
+    ingeschr_niet_nat_prs innp
+ON
+    innp.sc_identif = nnp.sc_identif
+LEFT JOIN
+    subject innp_subject
+ON
+    innp_subject.identif = innp.sc_identif
+WHERE
+    zr.kadaster_identif like 'NL.KAD.Tenaamstelling%';
   
