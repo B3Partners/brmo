70cbdbd7bc731f13fdf424e2d8c59a875468bd2e
Mark Prins <mark@b3partners.nl>
2016-08-23 17:48:10 +0200

    verbetering van de logging van fout conditie
    afvangen van de fout conditie zodat de status van het proces goed kan worden gezet
    
    
    close #206

--- brmo-service/src/main/java/nl/b3p/brmo/service/scanner/BerichtTransformatieUitvoeren.java
+++ brmo-service/src/main/java/nl/b3p/brmo/service/scanner/BerichtTransformatieUitvoeren.java
@@ -3,11 +3,15 @@
  */
 package nl.b3p.brmo.service.scanner;
 
+import java.util.Calendar;
 import java.util.Date;
 import javax.sql.DataSource;
 import nl.b3p.brmo.loader.BrmoFramework;
 import nl.b3p.brmo.loader.util.BrmoException;
 import nl.b3p.brmo.persistence.staging.AutomatischProces;
+import static nl.b3p.brmo.persistence.staging.AutomatischProces.ProcessingStatus.ERROR;
+import static nl.b3p.brmo.persistence.staging.AutomatischProces.ProcessingStatus.PROCESSING;
+import static nl.b3p.brmo.persistence.staging.AutomatischProces.ProcessingStatus.WAITING;
 import nl.b3p.brmo.persistence.staging.BerichtTransformatieProces;
 import nl.b3p.brmo.service.util.ConfigUtil;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -31,9 +35,12 @@ public class BerichtTransformatieUitvoeren extends AbstractExecutableProces {
 
     private ProgressUpdateListener l;
 
+    private boolean transformErrorOccured = false;
+
     public BerichtTransformatieUitvoeren(BerichtTransformatieProces config) {
         this.config = config;
     }
+
 
     @Override
     public void execute() throws BrmoException {
@@ -48,6 +55,8 @@ public class BerichtTransformatieUitvoeren extends AbstractExecutableProces {
 
             @Override
             public void exception(Throwable t) {
+                // afvangen van de fout conditie zodat de status van het proces goed kan worden gezet
+                transformErrorOccured = true;
                 log.error(t);
             }
 
@@ -64,11 +73,27 @@ public class BerichtTransformatieUitvoeren extends AbstractExecutableProces {
     @Override
     public void execute(ProgressUpdateListener listener) {
         this.l = listener;
-
         l.updateStatus("Initialiseren...");
         l.addLog(String.format("Initialiseren... %tc", new Date()));
-        this.config.setStatus(AutomatischProces.ProcessingStatus.PROCESSING);
+        this.config.setStatus(PROCESSING);
+        this.config.setLastrun(new Date());
+        Stripersist.getEntityManager().merge(config);
         Stripersist.getEntityManager().flush();
+
+        final StringBuilder sb = new StringBuilder(AutomatischProces.LOG_NEWLINE);
+        String oldLog = config.getLogfile();
+        if (oldLog != null) {
+            if (oldLog.length() > 2000) {
+                sb.append(oldLog.substring(oldLog.length() - 200));
+            } else {
+                sb.append(oldLog);
+            }
+        }
+
+        String msg = String.format("Het bericht transformatie proces met ID %d is gestart op %tc.", config.getId(), Calendar.getInstance());
+        log.info(msg);
+        l.addLog(msg);
+        sb.append(msg).append(AutomatischProces.LOG_NEWLINE);
 
         BrmoFramework brmo = null;
         try {
@@ -82,6 +107,8 @@ public class BerichtTransformatieUitvoeren extends AbstractExecutableProces {
             Thread t = brmo.toRsgb(new nl.b3p.brmo.loader.ProgressUpdateListener() {
                 @Override
                 public void total(long total) {
+                    sb.append("Totaal te transformeren: ")
+                            .append(total).append(AutomatischProces.LOG_NEWLINE);
                     l.total(total);
                 }
 
@@ -92,24 +119,38 @@ public class BerichtTransformatieUitvoeren extends AbstractExecutableProces {
 
                 @Override
                 public void exception(Throwable t) {
+                    sb.append("Fout tijdens transformeren: ")
+                            .append(t.getLocalizedMessage()).append(AutomatischProces.LOG_NEWLINE);
                     l.exception(t);
                 }
             });
             t.join();
 
-            this.config.setStatus(AutomatischProces.ProcessingStatus.WAITING);
-            this.config.setSamenvatting("Bericht transformatie is afgerond.");
-            this.config.setLastrun(new Date());
-            l.addLog("Bericht transformatie is afgerond.");
-        } catch (Throwable t) {
+            if (transformErrorOccured) {
+                this.config.setStatus(ERROR);
+                msg = "Handmatige transformatie vanuit de berichten pagina is noodzakelijk.";
+                sb.append(msg).append(AutomatischProces.LOG_NEWLINE);
+                log.warn(msg);
+                msg = String.format("Bericht transformatie proces met ID %d is niet succesvol afgerond op %tc.", config.getId(), Calendar.getInstance());
+                log.error(msg);
+            } else {
+                this.config.setStatus(WAITING);
+                msg = String.format("Bericht transformatie proces met ID %d is succesvol afgerond op %tc.", config.getId(), Calendar.getInstance());
+                log.info(msg);
+            }
+            sb.append(msg).append(AutomatischProces.LOG_NEWLINE);
+            l.addLog(msg);
+            this.config.setSamenvatting(msg);
+            this.config.setLogfile(sb.toString());
+        } catch (BrmoException | InterruptedException t) {
             log.error("Fout bij transformeren berichten naar RSGB", t);
             String m = "Fout bij transformeren berichten naar RSGB: " + ExceptionUtils.getMessage(t);
             if (t.getCause() != null) {
                 m += ", oorzaak: " + ExceptionUtils.getRootCauseMessage(t);
             }
+            this.config.setLogfile(sb.toString());
             this.config.updateSamenvattingEnLogfile(m);
-            this.config.setStatus(AutomatischProces.ProcessingStatus.ERROR);
-            this.config.setLastrun(new Date());
+            this.config.setStatus(ERROR);
         } finally {
             if (brmo != null) {
                 brmo.closeBrmoFramework();
