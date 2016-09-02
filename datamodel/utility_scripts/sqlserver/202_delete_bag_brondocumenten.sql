1bf3eb6f501ef7d625f3896378956baecf413d89
cvanlith <chrisvanlith@gmail.com>
2016-09-02 08:59:57 +0200

    bag brondocumenten verwijderen

---
+++ datamodel/utility_scripts/sqlserver/202_delete_bag_brondocumenten.sql
@@ -0,0 +1,4 @@
+﻿-- brondocument tabel kan niet in zijn geheel gewist worden
+-- omdat meerdere basisregistraties daar samenkomen
+delete from brondocument where tabel in ('verblijfsobject','woonplaats','standplaats','pand','ligplaats','openbareruimte','nummeraanduiding');
+
