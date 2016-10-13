af495baad544ea80c0c5459972dba0612ad604a6
Mark Prins <mark@b3partners.nl>
2016-10-13 18:01:09 +0200

    upgrade scripts voor code 23/24

--- datamodel/upgrade_scripts/1.3.6-1.4.0/postgresql/rsgb.sql
+++ datamodel/upgrade_scripts/1.3.6-1.4.0/postgresql/rsgb.sql
@@ -1,3 +1,21 @@
 --
 -- upgrade RSGB datamodel van 1.3.6 naar 1.4.0 (PostgreSQL)
--- 
+--
+-- upsert van de nieuwe waarden voor Aard Recht codelijst (issue#234)
+WITH new_values (id, txt) AS (VALUES
+        ('23','Opstalrecht Nutsvoorzieningen op gedeelte van perceel'),
+        ('24','Zakelijk recht (als bedoeld in artikel 5, lid 3, onder b)')
+    ), upsert AS (
+        UPDATE aard_recht_verkort m SET omschr = nv.txt
+        FROM new_values nv WHERE  m.aand = nv.id RETURNING m.* 
+    )
+INSERT INTO aard_recht_verkort (aand, omschr) SELECT id, txt FROM new_values WHERE NOT EXISTS (SELECT 1 FROM upsert up WHERE up.aand = new_values.id);
+                  
+WITH new_values (id, txt) AS (VALUES
+        ('23','Opstalrecht Nutsvoorzieningen op gedeelte van perceel'),
+        ('24','Zakelijk recht als bedoeld in artikel 5, lid 3, onder b, van de Belemmeringenwet Privaatrecht op gedeelte van perceel')
+    ), upsert AS (
+        UPDATE aard_verkregen_recht m SET omschr_aard_verkregenr_recht = nv.txt
+        FROM new_values nv WHERE  m.aand = nv.id RETURNING m.* 
+    )
+INSERT INTO aard_verkregen_recht (aand, omschr_aard_verkregenr_recht) SELECT id, txt FROM new_values WHERE NOT EXISTS (SELECT 1 FROM upsert up WHERE up.aand = new_values.id);
