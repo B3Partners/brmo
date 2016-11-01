8b7a9736cb6bf0df8284abca009d9e308d2b6a61
Mark Prins <mark@b3partners.nl>
2016-11-01 15:40:00 +0100

    Aanpassingen BAG views Oracle tbv. esri software

--- datamodel/utility_scripts/oracle/403_create_geotools_primarykey_metatable.sql
+++ datamodel/utility_scripts/oracle/403_create_geotools_primarykey_metatable.sql
@@ -5,7 +5,7 @@
 --
 -- zie: http://docs.geoserver.org/stable/en/user/data/database/primarykey.html
 --
--- NB let op de schema naam 'RSGBTEST' in dit bestand; die dient vervangen te worden
+-- NB let op de schema naam 'RSGB' in dit bestand; die dient mogelijk vervangen te worden
 -- NB de lijst views is mogelijk niet compleet, afhankelijk van installatie
 -- 
 CREATE TABLE gt_pk_metadata (
@@ -21,27 +21,41 @@ CREATE UNIQUE INDEX gt_pk_metadata_table_idx01 ON gt_pk_metadata (table_schema, 
 COMMENT ON TABLE gt_pk_metadata IS 'Primary key metadata tabel ten behoeve van Geoserver/Geotools';
 
 --
--- insert de primary key metadata voor de views
+-- insert de primary key metadata voor de views, NB OBJECTID is een gegenereerd veld (rijnummer)
 --
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_ADRES', 'FID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_ADRES_LIGPLAATS', 'FID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_ADRES_STANDPLAATS', 'FID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_ADRES_TOTAAL', 'FID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_BD_APP_RE_BIJ_PERCEEL', 'SC_KAD_IDENTIF', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_BD_KAD_PERCEEL_MET_APP', 'SC_KAD_IDENTIF', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_KAD_PERCEEL_EENVOUDIG', 'SC_KAD_IDENTIF', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_KAD_PERCEEL_IN_EIGENDOM', 'SC_KAD_IDENTIF', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_LIGPLAATS', 'SC_KAD_IDENTIF', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_MAP_KAD_PERCEEL', 'SC_KAD_IDENTIF', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_PAND_GEBRUIK_NIET_INGEMETEN', 'FID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_PAND_IN_GEBRUIK', 'FID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_STANDPLAATS', 'SC_IDENTIF', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_VERBLIJFSOBJECT', 'FID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_VERBLIJFSOBJECT_ALLES', 'FID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_VERBLIJFSOBJECT_GEVORMD', 'FID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_ADRES', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_ADRES_LIGPLAATS', 'FID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_LIGPLAATS_ALLES', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_STANDPLAATS_ALLES', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_ADRES_STANDPLAATS', 'FID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_ADRES_TOTAAL', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_BD_APP_RE_BIJ_PERCEEL', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_BD_KAD_PERCEEL_MET_APP', 'SC_KAD_IDENTIF', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_KAD_PERCEEL_EENVOUDIG', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_KAD_PERCEEL_IN_EIGENDOM', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_LIGPLAATS', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_MAP_KAD_PERCEEL', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_PAND_GEBRUIK_NIET_INGEMETEN', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_PAND_IN_GEBRUIK', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_STANDPLAATS', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_VERBLIJFSOBJECT', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_VERBLIJFSOBJECT_ALLES', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_VERBLIJFSOBJECT_GEVORMD', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_KAD_PERCEEL_ZR_ADRESSEN', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_BD_APP_RE_AND_KAD_PERCEEL', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'VM_KAD_EIGENARENKAART', 'OBJECTID', NULL, 'assigned', NULL);
+INSERT INTO gt_pk_metadata VALUES ('RSGB', 'V_BD_KAD_PERCEEL_MET_APP_VLAK', 'OBJECTID', NULL, 'assigned', NULL);
 
--- in onderstaande tabellen is OBJECTID een gegenereerd veld
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_KAD_PERCEEL_ZR_ADRESSEN', 'OBJECTID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_BD_APP_RE_AND_KAD_PERCEEL', 'OBJECTID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'VM_KAD_EIGENARENKAART', 'OBJECTID', NULL, 'assigned', NULL);
-INSERT INTO gt_pk_metadata VALUES ('RSGBTEST', 'V_BD_KAD_PERCEEL_MET_APP_VLAK', 'OBJECTID', NULL, 'assigned', NULL);
+--SELECT table_name
+--  FROM user_tab_columns
+-- WHERE table_name IN (select VIEW_NAME from USER_VIEWS)
+-- AND COLUMN_NAME='OBJECTID'
+-- ORDER BY table_name;
+
+
+
+
+
+
+
+
