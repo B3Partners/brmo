e584df4830fcc3558f2f6f98abb5a8bb0f6229b3
Mark Prins <mark@b3partners.nl>
2016-11-14 14:21:55 +0100

    robuuster maken van de datumconversie functie

--- datamodel/utility_scripts/unsupported/overijssel/datumfunctie.sql
+++ datamodel/utility_scripts/unsupported/overijssel/datumfunctie.sql
@@ -1,13 +1,12 @@
-CREATE OR REPLACE FUNCTION f_datum(
-    p_dat VARCHAR2)
+create or replace FUNCTION F_DATUM(p_dat VARCHAR2)
   RETURN DATE
 IS
   l_dat DATE;
 BEGIN
-  IF (SUBSTR(p_dat, 5,1)    = '-') THEN
-    l_dat                  := to_date(p_dat, 'yyyy-mm-dd');
-  elsif (SUBSTR(p_dat, 6,1) = '-') THEN
-    l_dat                  := to_date(p_dat, 'dd-mm-yyyy');
+  IF (SUBSTR(p_dat, 8,1)    = '-') THEN
+    l_dat := to_date(p_dat, 'yyyy-mm-dd');
+  ELSIF (SUBSTR(p_dat, 6,1) = '-') THEN
+    l_dat := to_date(p_dat, 'dd-mm-yyyy');
   ELSE
     l_dat := NULL;
   END IF;
