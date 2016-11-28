30d7646caf07deb883c56cc754e82f1907829896
Meine Toonen <toonen.meine@gmail.com>
2016-11-28 16:00:19 +0100

    Removed unused xsd's

--- src/main/resources/nl/b3p/top250nlparser/imroGML/XSD-Resolver/XSD-Resolver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm generated/*.xml
-rm finalized/*.xsd
-rm consolidated/*.xsd
-java -Xmx1024m -jar saxon/saxon9he.jar -o:generated/temp.xml -s:input/configuration.xml -xsl:xslt/resolveXSDs.xslt versie=
