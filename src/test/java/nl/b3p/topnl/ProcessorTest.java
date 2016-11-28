f553b3750d7747d53193350478a5233da3532688
Meine Toonen <toonen.meine@gmail.com>
2016-11-28 16:40:02 +0100

    Added testpackage

---
+++ src/test/java/nl/b3p/topnl/ProcessorTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2016 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.b3p.topnl;
+
+import java.io.InputStream;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author meine
+ */
+public class ProcessorTest {
+    
+    private Processor instance = new Processor();
+    
+    public ProcessorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of Processor method, of class Processor.
+     */
+    @Test
+    public void testProcessor() {
+        System.out.println("Processor");
+        instance.Processor();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of parse method, of class Processor.
+     */
+    @Test
+    public void testParse() {
+        System.out.println("parse");
+        InputStream in = null;
+        Processor instance = new Processor();
+        instance.parse(in);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convert method, of class Processor.
+     */
+    @Test
+    public void testConvert() {
+        System.out.println("convert");
+        Processor instance = new Processor();
+        instance.convert();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of save method, of class Processor.
+     */
+    @Test
+    public void testSave() {
+        System.out.println("save");
+        Processor instance = new Processor();
+        instance.save();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
