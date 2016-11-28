77910e52a3cc8c624170a177ef48ac8e6a6f58a6
Meine Toonen <toonen.meine@gmail.com>
2016-11-28 15:58:48 +0100

    WIP. setup of global application layout

---
+++ src/main/java/nl/b3p/topnl/Processor.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.b3p.topnl;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author Meine Toonen
+ */
+public class Processor {
+    
+    
+    public void Processor(){
+        
+    }
+    
+    public void parse (InputStream in){
+        
+    }
+    
+    public void convert(){
+        
+    }
+    
+    public void save(){
+        
+    }
+    
+}
---
+++ src/main/java/nl/b3p/topnl/Top250NLConverter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.b3p.topnl;
+
+/**
+ *
+ * @author Meine Toonen
+ */
+public class Top250NLConverter extends Converter{
+    
+}
