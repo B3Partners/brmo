84c738ef88e8a502b7c06eb226c15d11b185a6ff
Meine Toonen <toonen.meine@gmail.com>
2016-12-06 11:14:11 +0100

    Tests for all entities added

--- src/test/java/nl/b3p/topnl/DatabaseTest.java
+++ src/test/java/nl/b3p/topnl/DatabaseTest.java
@@ -17,7 +17,10 @@
 package nl.b3p.topnl;
 
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.io.WKTReader;
+import java.math.BigInteger;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -25,8 +28,18 @@ import java.util.Arrays;
 import java.util.Collection;
 import nl.b3p.topnl.converters.DbUtilsGeometryColumnConverter;
 import nl.b3p.topnl.entities.FunctioneelGebied;
+import nl.b3p.topnl.entities.Gebouw;
+import nl.b3p.topnl.entities.GeografischGebied;
 import nl.b3p.topnl.entities.Hoogte;
+import nl.b3p.topnl.entities.Inrichtingselement;
+import nl.b3p.topnl.entities.Plaats;
+import nl.b3p.topnl.entities.RegistratiefGebied;
+import nl.b3p.topnl.entities.Relief;
+import nl.b3p.topnl.entities.Spoorbaandeel;
+import nl.b3p.topnl.entities.Terrein;
 import nl.b3p.topnl.entities.TopNLEntity;
+import nl.b3p.topnl.entities.Waterdeel;
+import nl.b3p.topnl.entities.Wegdeel;
 import org.apache.commons.dbutils.BasicRowProcessor;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
@@ -101,6 +114,305 @@ public class DatabaseTest extends TestUtil{
         assertEquals(e.getHoogte(),real.getHoogte());
         assertEquals(p,real.getGeometrie());
     }
+
+    @Test
+    public void testSaveGebouw()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
+        System.out.println("save");
+        Geometry p = wkt.read("POINT (1 2)");
+        Gebouw e = new Gebouw();
+        getStandardTestTopNLEntity(e, type);
+        e.setGeometrie(p);
+        e.setHoogte(16.06);
+        e.setTypeGebouw("flat");
+        e.setStatus("heel");
+        e.setFysiekVoorkomen("vervallen");
+        e.setHoogteklasse("superduperhoog");
+        e.setSoortnaam("superflat");
+        e.setNaam("flatje");
+        e.setNaamFries("flaotjah");
+        
+        instance.save(e);
+
+        QueryRunner run = new QueryRunner(datasource);
+
+        ResultSetHandler<Gebouw> h = new BeanHandler<>(Gebouw.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
+
+        Gebouw real = run.query("SELECT * FROM "  + type.getType() + ".gebouw WHERE identificatie=?", h, identificatie);
+        assertNotNull("Insert failed", real);
+        testStandardTopNLEntity(real, e);
+        
+        assertEquals(e.getHoogte(), real.getHoogte());
+        assertEquals(p, real.getGeometrie());
+        assertEquals(e.getTypeGebouw(), real.getTypeGebouw());
+        assertEquals(e.getStatus(), real.getStatus());
+        assertEquals(e.getFysiekVoorkomen(), real.getFysiekVoorkomen());
+        assertEquals(e.getHoogteklasse(), real.getHoogteklasse());
+        assertEquals(e.getSoortnaam(), real.getSoortnaam());
+        assertEquals(e.getNaam(), real.getNaam());
+        assertEquals(e.getNaamFries(), real.getNaamFries());
+    }
+
+    @Test
+    public void testSaveGeografischGebied()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
+        System.out.println("save");
+        Geometry p = wkt.read("POINT (1 2)");
+        GeografischGebied e = new GeografischGebied();
+        getStandardTestTopNLEntity(e, type);
+        e.setGeometrie(p);
+        e.setTypeGeografischGebied("landgoed");
+        e.setNaamNL("Naamnl");
+        e.setNaamFries("NaamFrysk");
+        
+        instance.save(e);
+
+        QueryRunner run = new QueryRunner(datasource);
+
+        ResultSetHandler<GeografischGebied> h = new BeanHandler<>(GeografischGebied.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
+
+        GeografischGebied real = run.query("SELECT * FROM "  + type.getType() + ".geografischgebied WHERE identificatie=?", h, identificatie);
+        assertNotNull("Insert failed", real);
+        testStandardTopNLEntity(real, e);
+        
+        assertEquals(p, real.getGeometrie());
+        assertEquals(e.getNaamNL(), real.getNaamNL());
+        assertEquals(e.getNaamFries(), real.getNaamFries());
+        assertEquals(e.getTypeGeografischGebied(), real.getTypeGeografischGebied());
+    }
+
+    @Test
+    public void testSaveInrichtingselement()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
+        System.out.println("save");
+        Geometry p = wkt.read("POINT (1 2)");
+        Inrichtingselement e = new Inrichtingselement();
+        getStandardTestTopNLEntity(e, type);
+        e.setGeometrie(p);
+        e.setTypeInrichtingselement("Gesloten");
+        e.setHoogteniveau(new BigInteger("778"));
+        e.setSoortnaam("MaxPen");
+        e.setStatus("in gebruik");
+        
+        instance.save(e);
+
+        QueryRunner run = new QueryRunner(datasource);
+
+        ResultSetHandler<Inrichtingselement> h = new BeanHandler<>(Inrichtingselement.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
+
+        Inrichtingselement real = run.query("SELECT * FROM "  + type.getType() + ".inrichtingselement WHERE identificatie=?", h, identificatie);
+        assertNotNull("Insert failed", real);
+        testStandardTopNLEntity(real, e);
+        
+        assertEquals(p, real.getGeometrie());
+        assertEquals(e.getTypeInrichtingselement(), real.getTypeInrichtingselement());
+        assertEquals(e.getHoogteniveau(), real.getHoogteniveau());
+        assertEquals(e.getSoortnaam(), real.getSoortnaam());
+        assertEquals(e.getStatus(), real.getStatus());
+    }
+
+    @Test
+    public void testSavePlaats()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
+        System.out.println("save");
+        Geometry p = wkt.read("POINT (1 2)");
+        Plaats e = new Plaats();
+        getStandardTestTopNLEntity(e, type);
+        e.setGeometrie(p);
+        e.setTypeGebied("woonplaats");
+        e.setAantalInwoners(new BigInteger("16"));
+        e.setNaamOfficieel("Gouda");
+        e.setNaamNL("Gouda");
+        e.setNaamFries("mompelmompel");
+        instance.save(e);
+
+        QueryRunner run = new QueryRunner(datasource);
+
+        ResultSetHandler<Plaats> h = new BeanHandler<>(Plaats.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
+
+        Plaats real = run.query("SELECT * FROM "  + type.getType() + ".plaats WHERE identificatie=?", h, identificatie);
+        assertNotNull("Insert failed", real);
+        testStandardTopNLEntity(real, e);
+
+        assertEquals(p, real.getGeometrie());
+        assertEquals(e.getTypeGebied(), real.getTypeGebied());
+        assertEquals(e.getAantalInwoners(), real.getAantalInwoners());
+        assertEquals(e.getNaamOfficieel(), real.getNaamOfficieel());
+        assertEquals(e.getNaamNL(), real.getNaamNL());
+        assertEquals(e.getNaamFries(), real.getNaamFries());
+    }
+
+    @Test
+    public void testSaveRegistratiefGebied()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
+        System.out.println("save");
+        Geometry p = wkt.read("POINT (1 2)");
+        RegistratiefGebied e = new RegistratiefGebied();
+        getStandardTestTopNLEntity(e, type);
+        e.setGeometrie(p);
+        e.setTypeRegistratiefGebied("woonplaats");
+        e.setNummer("16");
+        e.setNaamOfficieel("Gouda");
+        e.setNaamNL("Gouda");
+        e.setNaamFries("mompelmompel");
+        instance.save(e);
+
+        QueryRunner run = new QueryRunner(datasource);
+
+        ResultSetHandler<RegistratiefGebied> h = new BeanHandler<>(RegistratiefGebied.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
+
+        RegistratiefGebied real = run.query("SELECT * FROM "  + type.getType() + ".registratiefgebied WHERE identificatie=?", h, identificatie);
+        assertNotNull("Insert failed", real);
+        testStandardTopNLEntity(real, e);
+
+        assertEquals(p, real.getGeometrie());
+        assertEquals(e.getTypeRegistratiefGebied(), real.getTypeRegistratiefGebied());
+        assertEquals(e.getNummer(), real.getNummer());
+        assertEquals(e.getNaamOfficieel(), real.getNaamOfficieel());
+        assertEquals(e.getNaamNL(), real.getNaamNL());
+        assertEquals(e.getNaamFries(), real.getNaamFries());
+    }
+    
+
+    @Test
+    public void testSaveRelief()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
+        System.out.println("save");
+        LineString p = (LineString)wkt.read("LineString(1 2, 3 4, 5 6)");
+        Relief e = new Relief();
+        getStandardTestTopNLEntity(e, type);
+        e.setGeometrie(p);
+        e.setTypeRelief("Berg");
+        e.setHoogteklasse("Superhoog");
+        e.setHoogteniveau(new BigInteger("666"));
+
+        instance.save(e);
+
+        QueryRunner run = new QueryRunner(datasource);
+
+        ResultSetHandler<Relief> h = new BeanHandler<>(Relief.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
+
+        Relief real = run.query("SELECT * FROM "  + type.getType() + ".relief WHERE identificatie=?", h, identificatie);
+        assertNotNull("Insert failed", real);
+        testStandardTopNLEntity(real, e);
+
+        assertEquals(p, real.getGeometrie());
+        assertEquals(e.getTypeRelief(), real.getTypeRelief());
+        assertEquals(e.getHoogteklasse(), real.getHoogteklasse());
+        assertEquals(e.getHoogteniveau(), real.getHoogteniveau());
+    }
+
+    @Test
+    public void testSaveSpoorbaandeel()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
+        System.out.println("save");
+        LineString p = (LineString)wkt.read("LineString(1 2, 3 4, 5 6)");
+        Spoorbaandeel e = new Spoorbaandeel();
+        getStandardTestTopNLEntity(e, type);
+        e.setGeometrie(p);
+        e.setTypeInfrastructuur("spoor");
+        e.setTypeSpoorbaan("treinspoor");
+        e.setFysiekVoorkomen("een treinspoort");
+        e.setSpoorbreedte("smal");
+        e.setAantalSporen("8");
+        e.setVervoerfunctie("Mensen");
+        e.setElektrificatie(true);
+        e.setStatus("in gebruik");
+        e.setBrugnaam("Brugge");
+        e.setTunnelnaam("Tunnele");
+        e.setBaanvaknaam("lalala");
+        e.setHoogteniveau(new BigInteger("1"));
+
+        instance.save(e);
+
+        QueryRunner run = new QueryRunner(datasource);
+
+        ResultSetHandler<Spoorbaandeel> h = new BeanHandler<>(Spoorbaandeel.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
+
+        Spoorbaandeel real = run.query("SELECT * FROM "  + type.getType() + ".spoorbaandeel WHERE identificatie=?", h, identificatie);
+        assertNotNull("Insert failed", real);
+        testStandardTopNLEntity(real, e);
+
+        assertEquals(p, real.getGeometrie());
+        assertEquals(e.getTypeInfrastructuur(), real.getTypeInfrastructuur());
+        assertEquals(e.getTypeSpoorbaan(), real.getTypeSpoorbaan());
+        assertEquals(e.getFysiekVoorkomen(), real.getFysiekVoorkomen());
+        assertEquals(e.getSpoorbreedte(), real.getSpoorbreedte());
+        assertEquals(e.getAantalSporen(), real.getAantalSporen());
+        assertEquals(e.getVervoerfunctie(), real.getVervoerfunctie());
+        assertEquals(e.getElektrificatie(), real.getElektrificatie());
+        assertEquals(e.getStatus(), real.getStatus());
+        assertEquals(e.getBrugnaam(), real.getBrugnaam());
+        assertEquals(e.getTunnelnaam(), real.getTunnelnaam());
+        assertEquals(e.getBaanvaknaam(), real.getBaanvaknaam());
+        assertEquals(e.getHoogteniveau(), real.getHoogteniveau());
+    }
+    
+
+    @Test
+    public void testSaveTerrein()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
+        System.out.println("save");
+        Polygon p = (Polygon)wkt.read("Polygon((1 2, 3 4, 5 6, 1 2))");
+        Terrein e = new Terrein();
+        getStandardTestTopNLEntity(e, type);
+        e.setGeometrie(p);
+        e.setTypeLandgebruik("festivalterrein");
+        e.setNaam("biddinghuizen");
+
+        instance.save(e);
+
+        QueryRunner run = new QueryRunner(datasource);
+
+        ResultSetHandler<Terrein> h = new BeanHandler<>(Terrein.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
+
+        Terrein real = run.query("SELECT * FROM "  + type.getType() + ".terrein WHERE identificatie=?", h, identificatie);
+        assertNotNull("Insert failed", real);
+        testStandardTopNLEntity(real, e);
+
+        assertEquals(p, real.getGeometrie());
+        assertEquals(e.getTypeLandgebruik(), real.getTypeLandgebruik());
+        assertEquals(e.getNaam(), real.getNaam());
+    }
+
+    @Test
+    public void testWaterdeel()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
+        System.out.println("save");
+        Geometry p = wkt.read("Polygon((1 2, 3 4, 5 6, 1 2))");
+        Waterdeel e = new Waterdeel();
+        getStandardTestTopNLEntity(e, type);
+        e.setGeometrie(p);
+        e.setTypeWater("Slotgracht");
+        e.setBreedteklasse("middenmoat");
+        e.setFysiekVoorkomen("nat");
+        e.setVoorkomen("nat");
+        e.setGetijdeinvloed(false);
+        e.setVaarwegklasse("gevaarlijk");
+        e.setNaamOfficieel("Slotgracht");
+        e.setNaamNL("Slotgracht");
+        e.setNaamFries("SloatGraogt");
+        e.setIsBAGnaam(false);
+        e.setSluisnaam("lala");
+        e.setBrugnaam("Ophaal");
+        e.setHoogteniveau(new BigInteger("8"));
+
+        instance.save(e);
+
+        QueryRunner run = new QueryRunner(datasource);
+
+        ResultSetHandler<Waterdeel> h = new BeanHandler<>(Waterdeel.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
+
+        Waterdeel real = run.query("SELECT * FROM "  + type.getType() + ".waterdeel WHERE identificatie=?", h, identificatie);
+        assertNotNull("Insert failed", real);
+        testStandardTopNLEntity(real, e);
+
+        assertEquals(p, real.getGeometrie());
+        assertEquals(e.getTypeWater(), real.getTypeWater());
+        assertEquals(e.getBreedteklasse(), real.getBreedteklasse());
+        assertEquals(e.getFysiekVoorkomen(), real.getFysiekVoorkomen());
+        assertEquals(e.getVoorkomen(), real.getVoorkomen());
+        assertEquals(e.getGetijdeinvloed(), real.getGetijdeinvloed());
+        assertEquals(e.getVaarwegklasse(), real.getVaarwegklasse());
+        assertEquals(e.getNaamOfficieel(), real.getNaamOfficieel());
+        assertEquals(e.getNaamNL(), real.getNaamNL());
+        assertEquals(e.getNaamFries(), real.getNaamFries());
+        assertEquals(e.getIsBAGnaam(), real.getIsBAGnaam());
+        assertEquals(e.getSluisnaam(), real.getSluisnaam());
+        assertEquals(e.getBrugnaam(), real.getBrugnaam());
+        assertEquals(e.getHoogteniveau(), real.getHoogteniveau());
+    }
     
     @Test
     public void testSaveFunctioneelgebied()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
@@ -120,7 +432,7 @@ public class DatabaseTest extends TestUtil{
 
         ResultSetHandler<FunctioneelGebied> h = new BeanHandler<>(FunctioneelGebied.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
 
-        FunctioneelGebied real = run.query("SELECT * FROM "  + type.getType() + ".FunctioneelGebied WHERE identificatie=?", h, identificatie);
+        FunctioneelGebied real = run.query("SELECT * FROM "  + type.getType() + ".functioneelgebied WHERE identificatie=?", h, identificatie);
         assertNotNull("Insert failed", real);
         testStandardTopNLEntity(real, e);
         
@@ -129,6 +441,69 @@ public class DatabaseTest extends TestUtil{
         assertEquals(e.getNaamNL(),real.getNaamNL());
         assertEquals(e.getTypeFunctioneelGebied(),real.getTypeFunctioneelGebied());
         assertEquals(e.getSoortnaam(),real.getSoortnaam());
+    }
+    
+    @Test
+    public void testSaveWegdeel()throws SQLException, ParseException, com.vividsolutions.jts.io.ParseException {
+        System.out.println("save");
+        Geometry p = wkt.read("POINT (1 2)");
+        Wegdeel e = new Wegdeel();
+        getStandardTestTopNLEntity(e, type);
+        e.setGeometrie(p);
+        e.setTypeInfrastructuur("Snelweg");
+        e.setTypeWeg("snelweg");
+        e.setHoofdverkeersgebruik("auto's");
+        e.setFysiekVoorkomen("666 baansweg");
+        e.setVerhardingsbreedteklasse("Vrij hard");
+        e.setGescheidenRijbaan(false);
+        e.setVerhardingstype("asfalt");
+        e.setAantalRijstroken(new BigInteger("666"));
+        e.setHoogteniveau(BigInteger.ONE);
+        e.setStatus("in gebruik");
+        e.setNaam("Route 666");
+        e.setIsBAGnaam(false);
+        e.setaWegnummer("666");
+        e.setnWegnummer("667");
+        e.seteWegnummer("668");
+        e.setsWegnummer("669");
+        e.setAfritnummer("8");
+        e.setAfritnaam("Afrit 8");
+        e.setKnooppuntnaam("Knooppunt lucifer");
+        e.setBrugnaam("Vagevuur");
+        e.setTunnelnaam("Hades");
+        
+        instance.save(e);
+
+        QueryRunner run = new QueryRunner(datasource);
+
+        ResultSetHandler<Wegdeel> h = new BeanHandler<>(Wegdeel.class, new BasicRowProcessor(new DbUtilsGeometryColumnConverter(instance.getGjc())));
+
+        Wegdeel real = run.query("SELECT * FROM "  + type.getType() + ".wegdeel WHERE identificatie=?", h, identificatie);
+        assertNotNull("Insert failed", real);
+        testStandardTopNLEntity(real, e);
+
+        assertEquals(p, real.getGeometrie());
+        assertEquals(e.getTypeInfrastructuur(), real.getTypeInfrastructuur());
+        assertEquals(e.getTypeWeg(), real.getTypeWeg());
+        assertEquals(e.getHoofdverkeersgebruik(), real.getHoofdverkeersgebruik());
+        assertEquals(e.getFysiekVoorkomen(), real.getFysiekVoorkomen());
+        assertEquals(e.getVerhardingsbreedteklasse(), real.getVerhardingsbreedteklasse());
+        assertEquals(e.getGescheidenRijbaan(), real.getGescheidenRijbaan());
+        assertEquals(e.getVerhardingstype(), real.getVerhardingstype());
+        assertEquals(e.getAantalRijstroken(), real.getAantalRijstroken());
+        assertEquals(e.getHoogteniveau(), real.getHoogteniveau());
+        assertEquals(e.getStatus(), real.getStatus());
+        assertEquals(e.getNaam(), real.getNaam());
+        assertEquals(e.getIsBAGnaam(), real.getIsBAGnaam());
+        assertEquals(e.getaWegnummer(), real.getaWegnummer());
+        assertEquals(e.getnWegnummer(), real.getnWegnummer());
+        assertEquals(e.geteWegnummer(), real.geteWegnummer());
+        assertEquals(e.getsWegnummer(), real.getsWegnummer());
+        assertEquals(e.getAfritnummer(), real.getAfritnummer());
+        assertEquals(e.getAfritnaam(), real.getAfritnaam());
+        assertEquals(e.getKnooppuntnaam(), real.getKnooppuntnaam());
+        assertEquals(e.getBrugnaam(), real.getBrugnaam());
+        assertEquals(e.getTunnelnaam(), real.getTunnelnaam());
     }
     
     public void getStandardTestTopNLEntity(TopNLEntity e,TopNLType type) throws ParseException {
