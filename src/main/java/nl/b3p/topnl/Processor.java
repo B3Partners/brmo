8c63876b4424e645b1bacbd87b142a18cf4bfac2
Meine Toonen <toonen.meine@gmail.com>
2016-12-07 13:24:24 +0100

    WIP: streaming xml reader implementation

--- src/main/java/nl/b3p/topnl/Processor.java
+++ src/main/java/nl/b3p/topnl/Processor.java
@@ -20,16 +20,22 @@ import com.vividsolutions.jts.io.ParseException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.transform.TransformerException;
 import nl.b3p.topnl.converters.Converter;
 import nl.b3p.topnl.converters.ConverterFactory;
 import nl.b3p.topnl.entities.TopNLEntity;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.xml.sax.SAXException;
 
 /**
@@ -37,6 +43,8 @@ import org.xml.sax.SAXException;
  * @author Meine Toonen
  */
 public class Processor {
+    protected final static Log log = LogFactory.getLog(Processor.class);
+
 
     private Database database;
     private ConverterFactory converterFactory;
@@ -47,13 +55,44 @@ public class Processor {
         converterFactory = new ConverterFactory();
     }
   
-    public Object parse(InputStream in, TopNLType type) throws JAXBException {
+   public Object parse(InputStream in, TopNLType type) throws JAXBException {
         Unmarshaller jaxbUnmarshaller = converterFactory.getContext(type).createUnmarshaller();
         JAXBElement o = (JAXBElement) jaxbUnmarshaller.unmarshal(in);
 
         Object value = o.getValue();
 
         return value;
+    }
+    
+    public Object parseStreaming (InputStream in, TopNLType type) throws  JAXBException{
+        try {
+            Unmarshaller jaxbUnmarshaller = converterFactory.getContext(type).createUnmarshaller();
+            
+            List list = new ArrayList();
+            XMLInputFactory xif = XMLInputFactory.newFactory();
+            
+            XMLStreamReader xsr = xif.createXMLStreamReader(in);
+            
+            while (xsr.hasNext()) {
+                int eventType = xsr.next();
+                
+                if (eventType == XMLStreamReader.START_ELEMENT) {
+                    System.out.println(xsr.getLocalName());
+                    if (xsr.getLocalName().equals("FeatureMember")) {
+                        
+                        
+                        JAXBElement jb = (JAXBElement)jaxbUnmarshaller.unmarshal(xsr);
+                        list.add(jb.getValue());
+                    }
+                }
+            }
+            
+            xsr.close();
+            return list;
+        } catch (XMLStreamException ex) {
+            log.error("cannot correctly stream xml file:", ex);
+        }
+        return null;
     }
     
     public List<TopNLEntity> convert(Object jaxbObject, TopNLType type)  throws IOException, SAXException, ParserConfigurationException, TransformerException{
