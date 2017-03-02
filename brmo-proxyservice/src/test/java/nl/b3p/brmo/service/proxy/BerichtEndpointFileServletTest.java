0a2512930b41b65b7da83f3a33c6a7119451b786
Mark Prins <mark@b3partners.nl>
2017-03-02 16:43:19 +0100

    randomize test directory

--- brmo-proxyservice/src/test/java/nl/b3p/brmo/service/proxy/BerichtEndpointFileServletTest.java
+++ brmo-proxyservice/src/test/java/nl/b3p/brmo/service/proxy/BerichtEndpointFileServletTest.java
@@ -13,6 +13,9 @@ import org.junit.Before;
 import org.junit.Test;
 import java.io.File;
 import java.nio.file.Files;
+import java.security.SecureRandom;
+import java.math.BigInteger;
+import java.util.Hashtable;
 import org.apache.commons.io.FileUtils;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -24,22 +27,26 @@ import static org.junit.Assert.assertTrue;
  */
 public class BerichtEndpointFileServletTest {
 
-    private String SAVE_DIR = "/brk";
+    private String saveDir = "/bericht-post-test-";
     private ServletUnitClient client;
     private ServletRunner sr;
 
     @Before
     public void setUp() throws Exception {
-        sr = new ServletRunner(BerichtEndpointFileServletTest.class.getResourceAsStream("/web.xml"));
-        sr.registerServlet("BRK_file", BerichtEndpointFileServlet.class.getName());
+        saveDir = System.getProperty("java.io.tmpdir", "/tmp") + saveDir + new BigInteger(130, new SecureRandom()).toString(32);
+        // sr = new ServletRunner(BerichtEndpointFileServletTest.class.getResourceAsStream("/web.xml"));
+        sr = new ServletRunner();
+        Hashtable<String, String> servletParams = new Hashtable();
+        servletParams.put("save_dir", saveDir);
+        sr.registerServlet("/post/brk", BerichtEndpointFileServlet.class.getName(), servletParams);
         client = sr.newClient();
-        SAVE_DIR = System.getProperty("java.io.tmpdir", "/tmp") + SAVE_DIR;
     }
 
     @After
     public void tearDown() throws Exception {
         sr.shutDown();
-        FileUtils.cleanDirectory(new File(SAVE_DIR));
+        FileUtils.cleanDirectory(new File(saveDir));
+        FileUtils.deleteQuietly(new File(saveDir));
     }
 
     @Test
@@ -49,9 +56,9 @@ public class BerichtEndpointFileServletTest {
         assertNotNull("No response received", response);
         assertEquals("Response not OK", 200, response.getResponseCode());
 
-        // servlet scrijft naar SAVE_DIR, /tmp/brk/post-op_<yyyy-MM-dd_HH-mm-ss-SSS>_<randomuniek>.xml
+        // servlet scrijft naar saveDir, /tmp/brk/post-op_<yyyy-MM-dd_HH-mm-ss-SSS>_<randomuniek>.xml
         File expected = new File(BerichtEndpointFileServletTest.class.getResource("/web.xml").getFile());
-        File actual = Files.newDirectoryStream(new File(SAVE_DIR).toPath(), "*.{xml}").iterator().next().toFile();
+        File actual = Files.newDirectoryStream(new File(saveDir).toPath(), "*.{xml}").iterator().next().toFile();
 
         assertTrue("Bestand begint niet met 'post-op", actual.getName().startsWith("post-op"));
         assertTrue("File inhoud niet gelijk", FileUtils.contentEquals(
