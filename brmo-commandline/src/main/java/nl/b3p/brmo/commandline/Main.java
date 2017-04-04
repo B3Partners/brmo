ac1ac5fe06491043d8899a2dbb2a84fca730ee16
Mark Prins <mark@b3partners.nl>
2017-04-04 09:37:54 +0200

    [Jenkins] test procedure tweaks voor Oracle

--- brmo-commandline/src/main/java/nl/b3p/brmo/commandline/Main.java
+++ brmo-commandline/src/main/java/nl/b3p/brmo/commandline/Main.java
@@ -17,8 +17,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
