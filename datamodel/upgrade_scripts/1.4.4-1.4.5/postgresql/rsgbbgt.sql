791c66829f44bf9441b0e458f79fde772867cf2e
Mark Prins <mark@b3partners.nl>
2017-04-19 14:10:39 +0200

    aanpassingen rsgb bgt schema voor verwerking van buurt/wijk/openbareruimte

--- datamodel/upgrade_scripts/1.4.4-1.4.5/postgresql/rsgbbgt.sql
+++ datamodel/upgrade_scripts/1.4.4-1.4.5/postgresql/rsgbbgt.sql
@@ -4,3 +4,56 @@
 
 -- versienummer update
 UPDATE brmo_metadata SET waarde='1.4.5' WHERE naam='brmoversie';
+
+-- Klasse: Buurt
+CREATE TABLE buurt (
+        identif character varying(255) NOT NULL,
+        dat_beg_geldh date,
+        datum_einde_geldh date,
+        relve_hoogteligging integer,
+        bgt_status character varying(255),
+        plus_status character varying(255),
+        buurtcode character varying(255),
+        naam character varying(255),
+        geom2d geometry(MULTIPOLYGON,28992),
+        wijk character varying(255),
+        bijwerkdatum date,
+        PRIMARY KEY (identif)
+);
+
+CREATE INDEX buurt_geom2d_idx on buurt USING GIST (geom2d);
+
+-- Klasse: Wijk
+CREATE TABLE wijk (
+        identif character varying(255) NOT NULL,
+        dat_beg_geldh date,
+        datum_einde_geldh date,
+        relve_hoogteligging integer,
+        bgt_status character varying(255),
+        plus_status character varying(255),
+        wijkcode character varying(255),
+        naam character varying(255),
+        geom2d geometry(MULTIPOLYGON,28992),
+        stadsdeel character varying(255),
+        bijwerkdatum date,
+        PRIMARY KEY (identif)
+);
+
+CREATE INDEX wijk_geom2d_idx on wijk USING GIST (geom2d);
+
+-- Klasse: OpenbareRuimte
+CREATE TABLE openbareruimte (
+        identif character varying(255) NOT NULL,
+        dat_beg_geldh date,
+        datum_einde_geldh date,
+        relve_hoogteligging integer,
+        bgt_status character varying(255),
+        plus_status character varying(255),
+        naam character varying(255),
+        naam_id_opr character varying(255),
+        geom2d geometry(MULTIPOLYGON,28992),
+        bijwerkdatum date,
+        PRIMARY KEY (identif)
+);
+
+CREATE INDEX openbareruimte_geom2d_idx on openbareruimte USING GIST (geom2d);
--- datamodel/upgrade_scripts/1.4.4-1.4.5/sqlserver/rsgbbgt.sql
+++ datamodel/upgrade_scripts/1.4.4-1.4.5/sqlserver/rsgbbgt.sql
@@ -4,3 +4,56 @@
 
 -- versienummer update
 UPDATE brmo_metadata SET waarde='1.4.5' WHERE naam='brmoversie';
+
+-- Klasse: Buurt
+CREATE TABLE buurt (
+        identif varchar(255) NOT NULL,
+        dat_beg_geldh date,
+        datum_einde_geldh date,
+        relve_hoogteligging integer,
+        bgt_status varchar(255),
+        plus_status varchar(255),
+        buurtcode varchar(255),
+        naam varchar(255),
+        geom2d geometry,
+        wijk varchar(255),
+        bijwerkdatum date,
+        PRIMARY KEY (identif)
+);
+
+CREATE SPATIAL INDEX buurt_geom2d_idx ON buurt(geom2d) WITH ( BOUNDING_BOX = (12000,304000,280000,620000));
+
+-- Klasse: Wijk
+CREATE TABLE wijk (
+        identif varchar(255) NOT NULL,
+        dat_beg_geldh date,
+        datum_einde_geldh date,
+        relve_hoogteligging integer,
+        bgt_status varchar(255),
+        plus_status varchar(255),
+        wijkcode varchar(255),
+        naam varchar(255),
+        geom2d geometry,
+        stadsdeel varchar(255),
+        bijwerkdatum date,
+        PRIMARY KEY (identif)
+);
+
+CREATE SPATIAL INDEX wijk_geom2d_idx ON wijk(geom2d) WITH ( BOUNDING_BOX = (12000,304000,280000,620000));
+
+-- Klasse: OpenbareRuimte
+CREATE TABLE openbareruimte (
+        identif varchar(255) NOT NULL,
+        dat_beg_geldh date,
+        datum_einde_geldh date,
+        relve_hoogteligging integer,
+        bgt_status varchar(255),
+        plus_status varchar(255),
+        naam varchar(255),
+        naam_id_opr varchar(255),
+        geom2d geometry,
+        bijwerkdatum date,
+        PRIMARY KEY (identif)
+);
+
+CREATE SPATIAL INDEX openbareruimte_geom2d_idx ON openbareruimte(geom2d) WITH ( BOUNDING_BOX = (12000,304000,280000,620000));
