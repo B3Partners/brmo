c214fb1d2541e481519a64d910a1152d8f8a364a
Mark Prins <mprins@users.sf.net>
2017-05-02 17:07:29 +0200

    fix javadoc

--- brmo-loader/src/main/java/nl/b3p/brmo/loader/BrmoFramework.java
+++ brmo-loader/src/main/java/nl/b3p/brmo/loader/BrmoFramework.java
@@ -144,9 +144,9 @@ public class BrmoFramework {
     }
 
     /**
-     * stel sortering van berichten in.
+     * stel bericht sortering van berichten in.
      *
-     * @param orderBerichten {@code false{ voor stand, {@code true} voor mutaties
+     * @param orderBerichten {@code false} voor stand, {@code true} voor mutaties
      */
     public void setOrderBerichten(boolean orderBerichten) {
         this.orderBerichten = orderBerichten;
