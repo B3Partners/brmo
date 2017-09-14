7f104135de3bf4b76ba6d6eac242d068e50bdc9f
Mark Prins <mark@b3partners.nl>
2017-09-14 09:29:19 +0200

    volgende release is 1.5.0

--- datamodel/upgrade_scripts/1.4.5-1.4.6/sqlserver/staging.sql
+++
@@ -1,9 +0,0 @@
--- 
--- upgrade SQLserver STAGING datamodel van 1.4.5 naar 1.4.6 
---
-
-
--- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
-INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_1.4.5_naar_1.4.6','vorige versie was '||waarde FROM brmo_metadata WHERE naam='brmoversie';
--- versienummer update
-UPDATE brmo_metadata SET waarde='1.4.6' WHERE naam='brmoversie';
