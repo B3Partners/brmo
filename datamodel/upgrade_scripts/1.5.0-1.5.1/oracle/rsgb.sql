5f2b7dfc9d6fb626088c77075ca68e424a2cfce8
Mark Prins <mark@b3partners.nl>
2017-09-19 10:32:28 +0200

    Migratie bestanden aanmaken voor upgrade 1.5.0-1.5.1

---
+++ datamodel/upgrade_scripts/1.5.0-1.5.1/oracle/rsgb.sql
@@ -0,0 +1,9 @@
+-- 
+-- upgrade Oracle RSGB datamodel van 1.5.0 naar 1.5.1 
+--
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_1.5.0_naar_1.5.1','vorige versie was '||waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='1.5.1' WHERE naam='brmoversie';
