662a9a1a61e076e15e132efb100b570d694a8485
Meine Toonen <toonen.meine@gmail.com>
2017-11-19 15:16:20 +0100

    added scripts for using BRP data

---
+++ datamodel/extra_scripts/oracle/118_drop_constraints_brp.sql
@@ -0,0 +1,5 @@
+alter table subject drop constraint fk_sub_as_13;
+alter table subject drop constraint fk_sub_as_14;
+alter table subject drop constraint fk_sub_as_15;
+alter table subject drop constraint fk_sub_pa_as_4;
+alter table subject drop constraint fk_sub_vb_4;
