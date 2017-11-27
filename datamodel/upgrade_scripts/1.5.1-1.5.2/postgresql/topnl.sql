e93b603896fb5547cfcc887ea84058c4c1ee3fd7
Mark Prins <mark@b3partners.nl>
2017-11-27 17:13:51 +0100

    nagekomen werk van #392, voor upgrade naar 1.5.2 is het beter het PG schema nieuw aan te maken

--- datamodel/upgrade_scripts/1.5.1-1.5.2/postgresql/topnl.sql
+++ datamodel/upgrade_scripts/1.5.1-1.5.2/postgresql/topnl.sql
@@ -2,5 +2,4 @@
 -- upgrade PostgreSQL TOPNL datamodel van 1.5.1 naar 1.5.2
 --
 
-alter table top10nl.Spoorbaandeel add column puntGeometrie geometry (Point, 28992);
-alter table top10nl.plantopografie add column geometrie geometry (Geometry, 28992);
+-- vanwege de vele aanpassingen moeten de topnl schema's gedropped en nieuw aangemaakt worden
