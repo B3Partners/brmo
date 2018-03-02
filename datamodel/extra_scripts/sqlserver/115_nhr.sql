032bd757e8e213dca8d24369d6486c309e32aeff
Mark Prins <mark@b3partners.nl>
2018-03-02 09:36:02 +0100

    (mssql) gebruik benoemde FKs in tabel vestg_activiteit en benoemde PK voor tabel vestg_naam

--- datamodel/extra_scripts/sqlserver/115_nhr.sql
+++ datamodel/extra_scripts/sqlserver/115_nhr.sql
@@ -2,10 +2,12 @@
 alter table sbi_activiteit alter column omschr varchar(255);
 
 create table vestg_activiteit(
-    fk_vestg_nummer varchar(32) references vestg(sc_identif),
-    fk_sbi_activiteit_code varchar(6) references sbi_activiteit(sbi_code),
+    fk_vestg_nummer varchar(32),
+    fk_sbi_activiteit_code varchar(6),
     indicatie_hoofdactiviteit decimal(1,0),
-    primary key(fk_vestg_nummer, fk_sbi_activiteit_code)
+    primary key(fk_vestg_nummer, fk_sbi_activiteit_code),
+    constraint fkfk_vestg_nummer foreign key (fk_vestg_nummer) references vestg(sc_identif),
+    constraint fkfk_sbi_activiteit_code foreign key (fk_vestg_nummer) references sbi_activiteit(sbi_code)
 );
 
 ALTER TABLE vestg_naam
@@ -17,6 +19,6 @@ ALTER TABLE vestg_naam
   ALTER COLUMN fk_ves_sc_identif varchar(32) NOT NULL;
 
 GO
-  
+
 ALTER TABLE vestg_naam
-  ADD PRIMARY KEY (naam, fk_ves_sc_identif);
+  ADD CONSTRAINT pk_vestg_naam PRIMARY KEY (naam, fk_ves_sc_identif);
