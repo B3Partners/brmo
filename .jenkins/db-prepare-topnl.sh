397541b66afcf83c3481f94b6be6b1e473e59b46
Mark Prins <mprins@users.sf.net>
2018-03-06 18:21:41 +0100

    fix typefout

--- .jenkins/db-prepare-topnl.sh
+++ .jenkins/db-prepare-topnl.sh
@@ -11,7 +11,7 @@ sqlplus -l -S top100nl/top100nl@192.168.1.41:1521/DB01 < ./topparser/src/main/re
 echo "aanmaken top250nl schema"
 sqlplus -l -S top250nl/top250nl@192.168.1.41:1521/DB01 < ./topparser/src/main/resources/nl/b3p/topnl/database/oracletop250nl.sql
 
-echt "toebedelen grants aan JENKINS_TOPNL"
+echo "toebedelen grants aan JENKINS_TOPNL"
 sqlplus -l -S top10nl/top10nl@192.168.1.41:1521/DB01 < ./.jenkins/db-grant-topnl.sql
 sqlplus -l -S top50nl/top50nl@192.168.1.41:1521/DB01 < ./.jenkins/db-grant-topnl.sql
 sqlplus -l -S top100nl/top100nl@192.168.1.41:1521/DB01 < ./.jenkins/db-grant-topnl.sql
