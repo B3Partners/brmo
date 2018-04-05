49d5df6667686b069450c2b8a08b9f28ffe7703a
Mark Prins <mark@b3partners.nl>
2018-04-05 16:44:29 +0200

    oracle wijk update

--- .jenkins/db-prepare-rsgb.sh
+++ .jenkins/db-prepare-rsgb.sh
@@ -4,4 +4,4 @@
 ls -l ./datamodel/generated_scripts/
 sqlplus -l -S jenkins_rsgb/jenkins_rsgb@192.168.1.41:1521/DB01 < ./datamodel/generated_scripts/datamodel_oracle.sql
 sqlplus -l -S jenkins_rsgb/jenkins_rsgb@192.168.1.41:1521/DB01 < ./datamodel/utility_scripts/oracle/111a_update_gemeente_geom.sql
-#sqlplus jenkins_rsgb/jenkins_rsgb@192.168.1.41:1521/DB01 < ./datamodel/utility_scripts/oracle/113a_update_wijk_geom.sql
+sqlplus jenkins_rsgb/jenkins_rsgb@192.168.1.41:1521/DB01 < ./datamodel/utility_scripts/oracle/113a_update_wijk_geom.sql
