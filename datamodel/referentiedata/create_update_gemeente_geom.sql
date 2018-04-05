71b98e609c03c294e2b081dba07bfb6a07d26c8a
Mark Prins <mark@b3partners.nl>
2018-04-05 10:56:33 +0200

    mssql gemeente geom update 2018

--- datamodel/referentiedata/create_update_gemeente_geom.sql
+++ datamodel/referentiedata/create_update_gemeente_geom.sql
@@ -1,25 +1,26 @@
--- ogr2ogr -f "PostgreSQL" "PG:dbname=dingetjes host=localhost user=mark_postgis password=mark_postgis" bestuurlijkegrenzen/Gemeentegrenzen.gml -append -nln public.gemeenten2018 -select "code" -nlt PROMOTE_TO_MULTI
-
-
--- maakt geometry update scripts voor de rsgb gemeente tabel tbv 111b_update_gemeente_geom.sql
--- laad de gemeente kaart uit de bestuurlijke grenzen van pdok in een postgis database, gebruik
--- onderstaande om de update scripts te maken voor 111<?>_update_gemeente_geom.sql
+-- maakt geometry update scripts voor de rsgb gemeente tabel tbv 111a_update_gemeente_geom.sql
+--
+-- 1. laad de gemeente kaart uit de bestuurlijke grenzen van pdok in een postgis database
+-- ogr2ogr -f "PostgreSQL" "PG:dbname=dingetjes host=localhost user=mark_postgis password=mark_postgis" bestuurlijkegrenzen/Gemeentegrenzen.gml -append -nln public.gemeenten2018 -select "code,gemeentenaam" -nlt PROMOTE_TO_MULTI
+--
+-- 2. gebruik onderstaande om de update scripts te maken voor 111a_update_gemeente_geom.sql
 SELECT
 
+-- postgis blok
 -- 'UPDATE gemeente SET geom = ST_GeomFromEWKT(''' || ST_AsEWKT(wkb_geometry) || ''') WHERE code = ' || trim(leading '0' FROM code) || ';
--- ' || 'INSERT INTO gemeente (code,geom) SELECT '|| trim(leading '0' FROM code) ||',ST_GeomFromEWKT(''' || ST_AsEWKT(wkb_geometry) || ''') WHERE NOT EXISTS (SELECT 1 FROM gemeente WHERE code='||trim(leading '0' FROM code)||');'
---  as postgis_update
+-- ' || 'INSERT INTO gemeente (code,naam,geom) SELECT '|| trim(leading '0' FROM code) ||','''|| gemeentenaam ||''',ST_GeomFromEWKT(''' || ST_AsEWKT(wkb_geometry) || ''') WHERE NOT EXISTS (SELECT 1 FROM gemeente WHERE code='||trim(leading '0' FROM code)||');'
+--  as "--postgis gemeente 2018 geometrie update"
 
- 'MERGE gemeente AS target USING (VALUES (geometry::STGeomFromText(''' || ST_AsText(wkb_geometry) || ''',28992) as source (geom) on target.code = ' || trim(leading '0' FROM code) || '
+-- mssql blok
+ 'MERGE gemeente AS target USING (VALUES (geometry::STGeomFromText(''' || ST_AsText(wkb_geometry) || ''',28992))) AS source (geom) ON target.code = ' || trim(leading '0' FROM code) || '
    WHEN MATCHED THEN UPDATE SET geom = source.geom
-   WHEN NOT MATCHED THEN INSERT (code,geom) VALUES (' || trim(leading '0' FROM code) || ', source.geom);'
-as sqlserver_update
+   WHEN NOT MATCHED THEN INSERT (code,naam,geom) VALUES (' || trim(leading '0' FROM code) ||','''|| gemeentenaam ||''', source.geom);'
+as "--mssql gemeente 2018 geometrie update"
 
-
--- Oracle heeft handwerk nodig!
+-- oracle blok
+-- NB. Oracle heeft handwerk nodig! oracle heeft een max lengte van 32767 voor een string dus de wkt moet opgeknipt worden in stukken...
 -- todo `DECLARE wktA CLOB; wktB CLOB; wktC CLOB; wktD CLOB; wktE CLOB; BEGIN` aan het begin van het script plakken
 -- todo `END;` aan het eind van het export bestand plakken
--- oracle heeft een max lengte van 32767 voor een string dus de wkt moet opgeknipt worden in stukken...
 
 --'
 --  wktA := ''' || substr(ST_AsText(wkb_geometry), 0,32766)     || ''';
@@ -36,11 +37,10 @@ as sqlserver_update
 --
 --  MERGE INTO gemeente USING dual ON (CODE=' || trim(leading '0' FROM code) || ')
 --    WHEN MATCHED THEN UPDATE SET GEOM=SDO_GEOMETRY(wktA,28992)
---    WHEN NOT MATCHED THEN INSERT (CODE,GEOM) VALUES (' || trim(leading '0' FROM code) || ', SDO_GEOMETRY(wktA,28992));'
---as oracle__update
-
+--    WHEN NOT MATCHED THEN INSERT (CODE,NAAM,GEOM) VALUES (' || trim(leading '0' FROM code) || ','''|| gemeentenaam ||''', SDO_GEOMETRY(wktA,28992));'
+--as "--oracle gemeente 2018 geometrie update"
 
 FROM gemeenten2018
-WHERE code IN ('0584','0448','0289')
-;
+--WHERE code IN ('0003','0584','0448','0289')
+ORDER BY code;
 
