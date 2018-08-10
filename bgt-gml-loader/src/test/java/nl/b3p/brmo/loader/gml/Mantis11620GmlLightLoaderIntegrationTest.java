860dbf7693ec653f49d0c5fe685b6819fcaf5908
Mark Prins <mark@b3partners.nl>
2018-08-10 15:21:07 +0200

    falende testcase om mantis-11620 te illustreren
    hernoem naar integration test zodat de test in de juiste build fase wordt uitgevoerd:

---
+++ bgt-gml-loader/src/test/java/nl/b3p/brmo/loader/gml/Mantis11620GmlLightLoaderIntegrationTest.java
@@ -0,0 +1,90 @@
+package nl.b3p.brmo.loader.gml;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureStore;
+import org.geotools.jdbc.JDBCDataStore;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Testcases voor
+ * <a href="http://mantis.b3p.nl/view.php?id=11620">mantis-11620</a>. Deze
+ * testcase test ove er geen vervallen object in de tabel terecht komen. Om de
+ * tests te runnen gebruik je:
+ * {@code mvn -Dit.test=Mantis11620GmlLightLoaderIntegrationTest -Dtest.onlyITs=true integration-test -Poracle > target/oracle.log}
+ * voor bijvoorbeeld Oracle of
+ * {@code mvn -Dit.test=Mantis11620GmlLightLoaderIntegrationTest -Dtest.onlyITs=true integration-test -Ppostgresql > target/postgresql.log}
+ * voor Postgis.
+ *
+ * Maak een override properties file aan voor jouw eigen database omgeving met
+ * de naam {@code local.<DATABASETYPE>.properties} waarin je de properties
+ * overschrijft die in {@code src/test/resources/<DATABASETYPE>.properties} zijn
+ * vastgelegd.
+ *
+ * @author mprins
+ */
+public class Mantis11620GmlLightLoaderIntegrationTest extends TestingBase {
+
+    private BGTGMLLightLoader ldr;
+
+    private final Lock sequential = new ReentrantLock();
+
+    /**
+     * set up test object.
+     *
+     * @throws IOException als laden van property file mislukt
+     */
+    @Before
+    public void setUp() throws Exception {
+        loadProps();
+
+        sequential.lock();
+
+        ldr = new BGTGMLLightLoader();
+        ldr.setDbConnProps(params);
+        ldr.setCreateTables(false);
+
+        clearTables();
+    }
+
+    @After
+    public void resetDatabase() throws Exception {
+        clearTables();
+        sequential.unlock();
+    }
+
+    /**
+     * test parsen en laden van het test bestand met vervallen object. Het
+     * resultaat moet een lege tabel zijn (0 features geschreven).
+     *
+     * @throws Exception if any
+     */
+    @Test
+    public void testProcessGMLFile() throws Exception {
+        File gml = new File(BGTGMLLightLoaderIntegrationTest.class.getResource("/mantis-11620/bgt_wegdeel.gml").toURI());
+        int result = ldr.processGMLFile(gml);
+        int expected = 0;
+
+        assertEquals("Aantal geschreven features moet 0 zijn want object is vervallen.", expected, result);
+
+        // doublecheck met geotools want die hebben we toch al
+        JDBCDataStore dataStore = null;
+        try {
+            dataStore = (JDBCDataStore) DataStoreFinder.getDataStore(params);
+            FeatureStore store = (FeatureStore) dataStore.getFeatureSource("wegdeel");
+            assertEquals("Aantal features is groter dan verwacht. ", expected, store.getFeatures().size());
+            dataStore.dispose();
+        } finally {
+            if (dataStore != null) {
+                dataStore.dispose();
+            }
+        }
+    }
+}
