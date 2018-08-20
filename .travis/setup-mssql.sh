6686367432993d0ec58b42c2d22b627b170730b1
Mark Prins <mprins@users.sf.net>
2018-08-20 10:16:53 +0200

    Integratie testen tegen mssql 2017 op travis-ci
    - setup scripts
    - reactiveer mssql2016 test suite op appveyor, schakel testen tegen mssql2017 uit
    - optimaliseer maven download

---
+++ .travis/setup-mssql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+export PATH="$PATH:/opt/mssql-tools/bin"
+# set up staging db
+sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE staging" -d "master"
+sqlcmd -S localhost -U sa -P Password12! -d staging -i ./brmo-persistence/db/create-brmo-persistence-sqlserver.sql
+sqlcmd -S localhost -U sa -P Password12! -d staging -i ./brmo-persistence/db/01_create_indexes.sql
+sqlcmd -S localhost -U sa -P Password12! -d staging -i ./brmo-persistence/db/02_insert_default_user.sql
+sqlcmd -S localhost -U sa -P Password12! -d staging -i ./brmo-persistence/db/05_create_brmo_metadata_sqlserver.sql
+# set up rsgb tabellen
+export SQLCMDINI=.appveyor/init.sql
+sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE rsgb" -d "master"
+sqlcmd -r0 -x -b -S localhost -U sa -P Password12! -d "rsgb" -I -i ./datamodel/generated_scripts/datamodel_sqlserver.sql
+# set up rsgbbgt tabellen
+sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE bgttest" -d "master"
+sqlcmd -r0 -b -S localhost -I -U sa -P Password12! -d "bgttest" -i ./bgt-gml-loader/target/generated-resources/ddl/sqlserver/create_rsgb_bgt.sql
+# set up topnl tabellen
+sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE topnl" -d "master"
+sqlcmd -r0 -b -S localhost -I -U sa -P Password12! -d "topnl" -i ./topparser/src/main/resources/nl/b3p/topnl/database/sqlserver.sql
