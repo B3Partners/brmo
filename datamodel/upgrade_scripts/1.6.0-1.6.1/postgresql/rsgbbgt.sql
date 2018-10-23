f9ec7be21ad64168cd59b67df96cf4d5cad7a4c1
Mark Prins <mark@b3partners.nl>
2018-10-23 09:38:26 +0200

    Upgrade script om tabel stadsdeel toe te voegen

--- datamodel/upgrade_scripts/1.6.0-1.6.1/postgresql/rsgbbgt.sql
+++ datamodel/upgrade_scripts/1.6.0-1.6.1/postgresql/rsgbbgt.sql
@@ -2,6 +2,20 @@
 -- upgrade PostgreSQL RSGBBGT datamodel van 1.6.0 naar 1.6.1 
 --
 
+-- voeg tabel voor klasse Stadsdeel toe
+CREATE TABLE stadsdeel (
+        identif character varying(255) NOT NULL,
+        dat_beg_geldh date,
+        tijdstip_registratie timestamp,
+        relve_hoogteligging integer,
+        bgt_status character varying(255),
+        plus_status character varying(255),
+        naam character varying(255),
+        geom2d geometry(MULTIPOLYGON,28992),
+        PRIMARY KEY (identif)
+);
+
+CREATE INDEX stadsdeel_geom2d_idx on stadsdeel USING GIST (geom2d);
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_1.6.0_naar_1.6.1','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
