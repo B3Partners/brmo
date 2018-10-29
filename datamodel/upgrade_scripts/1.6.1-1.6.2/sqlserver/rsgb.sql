0bc0af6bf9110268abaf9b42d55b5a089806af96
Mark Prins <mark@b3partners.nl>
2018-10-29 13:51:08 +0100

    Migratie bestanden aanmaken voor upgrade 1.6.1-1.6.2

---
+++ datamodel/upgrade_scripts/1.6.1-1.6.2/sqlserver/rsgb.sql
@@ -0,0 +1,9 @@
+-- 
+-- upgrade SQLserver RSGB datamodel van 1.6.1 naar 1.6.2 
+--
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_1.6.1_naar_1.6.2','vorige versie was ' + waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='1.6.2' WHERE naam='brmoversie';
