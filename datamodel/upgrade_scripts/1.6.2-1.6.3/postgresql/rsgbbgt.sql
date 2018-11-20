de75224c65945a9389232a8575dfb7e68cd8215e
Mark Prins <mark@b3partners.nl>
2018-11-20 17:07:25 +0100

    Migratie bestanden aanmaken voor upgrade 1.6.2-1.6.3

---
+++ datamodel/upgrade_scripts/1.6.2-1.6.3/postgresql/rsgbbgt.sql
@@ -0,0 +1,9 @@
+-- 
+-- upgrade PostgreSQL RSGBBGT datamodel van 1.6.2 naar 1.6.3 
+--
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_1.6.2_naar_1.6.3','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='1.6.3' WHERE naam='brmoversie';
