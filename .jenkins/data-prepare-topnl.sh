b1d7f7e89d62bbb355c3bbb8da441ceb55b40d35
Mark Prins <mark@b3partners.nl>
2019-01-03 13:09:47 +0100

    een TopNl integratie test die alleen de parser aanroept, die is een stuk sneller dan de integratie test die de data ook nog in de database verwerkt

--- .jenkins/data-prepare-topnl.sh
+++ .jenkins/data-prepare-topnl.sh
@@ -6,3 +6,5 @@ wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "http:/
 wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "http://geodata.nationaalgeoregister.nl/top100nl/extract/chunkdata/top100nl_gml_filechunks.zip?formaat=gml" --output-document=./TOP100NL.zip
 wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "http://geodata.nationaalgeoregister.nl/top250nl/extract/kaartbladtotaal/top250nl.zip?formaat=gml" --output-document=./TOP250NL.zip
 ls *.zip | while read filename; do unzip -j -o -d "brmo-loader/src/test/resources/topnl/" "$filename" *.gml; done;
+# opruimen ongebruikte files
+rm brmo-loader/src/test/resources/topnl/Top100NL_0000{02..10}.gml
