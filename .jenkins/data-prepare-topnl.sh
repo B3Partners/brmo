83beff3df1bd2aaef5e80b6fd52a0d764afd63e0
Mark Prins <mark@b3partners.nl>
2019-01-03 15:08:43 +0100

    Update data-prepare-topnl.sh
    opruimen ongebruikte files, maakt de builds sneller

--- .jenkins/data-prepare-topnl.sh
+++ .jenkins/data-prepare-topnl.sh
@@ -6,5 +6,13 @@ wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "http:/
 wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "http://geodata.nationaalgeoregister.nl/top100nl/extract/chunkdata/top100nl_gml_filechunks.zip?formaat=gml" --output-document=./TOP100NL.zip
 wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "http://geodata.nationaalgeoregister.nl/top250nl/extract/kaartbladtotaal/top250nl.zip?formaat=gml" --output-document=./TOP250NL.zip
 ls *.zip | while read filename; do unzip -j -o -d "brmo-loader/src/test/resources/topnl/" "$filename" *.gml; done;
-# opruimen ongebruikte files
-rm brmo-loader/src/test/resources/topnl/Top100NL_0000{02..10}.gml
+# opruimen ongebruikte files, maakt de builds sneller
+rm brmo-loader/src/test/resources/topnl/Top100NL_000002.gml 
+rm brmo-loader/src/test/resources/topnl/Top100NL_000003.gml 
+rm brmo-loader/src/test/resources/topnl/Top100NL_000004.gml 
+rm brmo-loader/src/test/resources/topnl/Top100NL_000005.gml 
+rm brmo-loader/src/test/resources/topnl/Top100NL_000006.gml 
+rm brmo-loader/src/test/resources/topnl/Top100NL_000007.gml 
+rm brmo-loader/src/test/resources/topnl/Top100NL_000008.gml 
+rm brmo-loader/src/test/resources/topnl/Top100NL_000009.gml 
+rm brmo-loader/src/test/resources/topnl/Top100NL_000010.gml
