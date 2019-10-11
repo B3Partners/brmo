b6015fb0cf17ff6f18f1ec0d494f63e6b85cb11b
Mark Prins <mark@b3partners.nl>
2019-10-11 12:41:53 +0200

    upgrade scripts voor oracle en postgis om de oude views te droppen

--- datamodel/upgrade_scripts/1.6.3-1.6.4/oracle/rsgb.sql
+++ datamodel/upgrade_scripts/1.6.3-1.6.4/oracle/rsgb.sql
@@ -6,6 +6,43 @@ UPDATE aard_verkregen_recht SET omschr_aard_verkregenr_recht='Zakelijk recht als
 UPDATE aard_recht_verkort SET omschr='Zakelijk recht (als bedoeld in artikel 5, lid 3, onder b)' WHERE aand='10';
 UPDATE aard_recht_verkort SET omschr='Zakelijk recht op gedeelte van perceel (als bedoeld in artikel 5, lid 3, onder b)' WHERE aand='24';
 
+-- PR#617 verwijderen oude views
+DELETE FROM USER_SDO_GEOM_METADATA WHERE TABLE_NAME IN ('MB_KAD_ONRRND_ZK_ARCHIEF', 'MB_AVG_KOZ_RECHTH', 'MB_KOZ_RECHTH', 'MB_AVG_ZR_RECHTH', 'MB_ZR_RECHTH', 'MB_KAD_ONRRND_ZK_ADRES', 'MB_UTIL_APP_RE_KAD_PERCEEL', 'MB_AVG_SUBJECT', 'MB_SUBJECT', 'MB_BEN_OBJ_NEVENADRES', 'MB_BENOEMD_OBJ_ADRES', 'MB_PAND', 'MB_ADRES', 'VB_KAD_ONRRND_ZK_ARCHIEF', 'VB_AVG_KOZ_RECHTH', 'VB_KOZ_RECHTH', 'VB_AVG_ZR_RECHTH', 'VB_ZR_RECHTH', 'VB_UTIL_ZK_RECHT', 'VB_KAD_ONRRND_ZK_ADRES', 'VB_UTIL_APP_RE_KAD_PERCEEL', 'VB_UTIL_APP_RE_PARENT', 'VB_UTIL_APP_RE_PARENT_2', 'VB_UTIL_APP_RE_PARENT_3', 'VB_UTIL_APP_RE_SPLITSING', 'VB_AVG_SUBJECT', 'VB_SUBJECT', 'VB_BEN_OBJ_NEVENADRES', 'VB_BENOEMD_OBJ_ADRES', 'VB_PAND', 'VB_LIGPLAATS_ADRES', 'VB_STANDPLAATS_ADRES', 'VB_VBO_ADRES', 'VB_ADRES');
+DROP MATERIALIZED VIEW mb_kad_onrrnd_zk_archief;
+DROP MATERIALIZED VIEW mb_avg_koz_rechth;
+DROP MATERIALIZED VIEW mb_koz_rechth;
+DROP MATERIALIZED VIEW mb_avg_zr_rechth;
+DROP MATERIALIZED VIEW mb_zr_rechth;
+DROP MATERIALIZED VIEW mb_kad_onrrnd_zk_adres;
+DROP MATERIALIZED VIEW mb_util_app_re_kad_perceel;
+DROP MATERIALIZED VIEW mb_avg_subject;
+DROP MATERIALIZED VIEW mb_subject;
+DROP MATERIALIZED VIEW mb_ben_obj_nevenadres;
+DROP MATERIALIZED VIEW mb_benoemd_obj_adres;
+DROP MATERIALIZED VIEW mb_pand;
+DROP MATERIALIZED VIEW mb_adres;
+DROP VIEW vb_kad_onrrnd_zk_archief;
+DROP VIEW vb_avg_koz_rechth;
+DROP VIEW vb_koz_rechth;
+DROP VIEW vb_avg_zr_rechth;
+DROP VIEW vb_zr_rechth;
+DROP VIEW vb_util_zk_recht;
+DROP VIEW vb_kad_onrrnd_zk_adres;
+DROP VIEW vb_util_app_re_kad_perceel;
+DROP VIEW vb_util_app_re_parent;
+DROP VIEW vb_util_app_re_parent_2;
+DROP VIEW vb_util_app_re_parent_3;
+DROP VIEW vb_util_app_re_splitsing;
+DROP VIEW vb_avg_subject;
+DROP VIEW vb_subject;
+DROP view vb_ben_obj_nevenadres;
+DROP VIEW vb_benoemd_obj_adres;
+DROP VIEW vb_pand;
+DROP VIEW vb_ligplaats_adres;
+DROP VIEW vb_standplaats_adres;
+DROP VIEW vb_vbo_adres;
+DROP VIEW vb_adres;
+
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_1.6.3_naar_1.6.4','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
 -- versienummer update
--- datamodel/upgrade_scripts/1.6.3-1.6.4/postgresql/rsgb.sql
+++ datamodel/upgrade_scripts/1.6.3-1.6.4/postgresql/rsgb.sql
@@ -6,6 +6,42 @@ UPDATE aard_verkregen_recht SET omschr_aard_verkregenr_recht='Zakelijk recht als
 UPDATE aard_recht_verkort SET omschr='Zakelijk recht (als bedoeld in artikel 5, lid 3, onder b)' WHERE aand='10';
 UPDATE aard_recht_verkort SET omschr='Zakelijk recht op gedeelte van perceel (als bedoeld in artikel 5, lid 3, onder b)' WHERE aand='24';
 
+-- PR#617 verwijderen oude views
+DROP MATERIALIZED VIEW IF EXISTS mb_kad_onrrnd_zk_archief;
+DROP MATERIALIZED VIEW IF EXISTS mb_avg_koz_rechth;
+DROP MATERIALIZED VIEW IF EXISTS mb_koz_rechth;
+DROP MATERIALIZED VIEW IF EXISTS mb_avg_zr_rechth;
+DROP MATERIALIZED VIEW IF EXISTS mb_zr_rechth;
+DROP MATERIALIZED VIEW IF EXISTS mb_kad_onrrnd_zk_adres;
+DROP MATERIALIZED VIEW IF EXISTS mb_util_app_re_kad_perceel;
+DROP MATERIALIZED VIEW IF EXISTS mb_avg_subject;
+DROP MATERIALIZED VIEW IF EXISTS mb_subject;
+DROP MATERIALIZED VIEW IF EXISTS mb_ben_obj_nevenadres;
+DROP MATERIALIZED VIEW IF EXISTS mb_benoemd_obj_adres;
+DROP MATERIALIZED VIEW IF EXISTS mb_pand;
+DROP MATERIALIZED VIEW IF EXISTS mb_adres;
+DROP VIEW IF EXISTS vb_kad_onrrnd_zk_archief;
+DROP VIEW IF EXISTS vb_avg_koz_rechth;
+DROP VIEW IF EXISTS vb_koz_rechth;
+DROP VIEW IF EXISTS vb_avg_zr_rechth;
+DROP VIEW IF EXISTS vb_zr_rechth;
+DROP VIEW IF EXISTS vb_util_zk_recht;
+DROP VIEW IF EXISTS vb_kad_onrrnd_zk_adres;
+DROP VIEW IF EXISTS vb_util_app_re_kad_perceel;
+DROP VIEW IF EXISTS vb_util_app_re_parent;
+DROP VIEW IF EXISTS vb_util_app_re_parent_2;
+DROP VIEW IF EXISTS vb_util_app_re_parent_3;
+DROP VIEW IF EXISTS vb_util_app_re_splitsing;
+DROP VIEW IF EXISTS vb_avg_subject;
+DROP VIEW IF EXISTS vb_subject;
+DROP view IF EXISTS vb_ben_obj_nevenadres;
+DROP VIEW IF EXISTS vb_benoemd_obj_adres;
+DROP VIEW IF EXISTS vb_pand;
+DROP VIEW IF EXISTS vb_ligplaats_adres;
+DROP VIEW IF EXISTS vb_standplaats_adres;
+DROP VIEW IF EXISTS vb_vbo_adres;
+DROP VIEW IF EXISTS vb_adres;
+
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_1.6.3_naar_1.6.4','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
 -- versienummer update
