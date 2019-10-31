eec64213a266afc50d414e62662cf39b3ff3dd72
Meine Toonen <toonen.meine@gmail.com>
2019-10-31 15:12:52 +0100

    moar indices!

--- datamodel/extra_scripts/postgresql/122_idx_gebr_doel.sql
+++ datamodel/extra_scripts/postgresql/122_idx_gebr_doel.sql
@@ -3,3 +3,4 @@ CREATE INDEX idx_gebrdoel
     (fk_gbo_sc_identif ASC NULLS LAST)
     TABLESPACE pg_default;
 
+CREATE INDEX mb_kad_onrrnd_zak_archief_overgegaan_in_idx ON public.mb_kad_onrrnd_zk_archief USING btree (overgegaan_in);
