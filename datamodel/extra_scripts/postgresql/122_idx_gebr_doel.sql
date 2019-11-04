eae32859c3f751390a4355fcddebc158528c509f
Mark Prins <markprins@b3partners.nl>
2019-11-04 13:30:58 +0100

    Update 122_idx_gebr_doel.sql
    verwijder plublic schema prefix

--- datamodel/extra_scripts/postgresql/122_idx_gebr_doel.sql
+++ datamodel/extra_scripts/postgresql/122_idx_gebr_doel.sql
@@ -1,4 +1,4 @@
 CREATE INDEX idx_gebrdoel
-    ON public.gebouwd_obj_gebruiksdoel USING btree
+    ON gebouwd_obj_gebruiksdoel USING btree
     (fk_gbo_sc_identif ASC NULLS LAST)
     TABLESPACE pg_default;
