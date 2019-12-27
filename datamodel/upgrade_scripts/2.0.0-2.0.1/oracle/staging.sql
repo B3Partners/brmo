c3cae5270fbf651bfe8bac8693451e4295a04e9f
Mark Prins <mark@b3partners.nl>
2019-12-27 08:57:04 +0100

    vul de bestand_naam_hersteld uit afgifte gegevens
    zie #761

--- datamodel/upgrade_scripts/2.0.0-2.0.1/oracle/staging.sql
+++ datamodel/upgrade_scripts/2.0.0-2.0.1/oracle/staging.sql
@@ -3,6 +3,18 @@
 --
 
 
+
+
+-- #761 vul de bestand_naam_hersteld uit afgifte gegevens
+UPDATE
+    laadproces
+SET
+    bestand_naam_hersteld = SUBSTR(bestand_naam, 1, INSTR(bestand_naam, '.zip')+ 4)
+WHERE
+    soort = 'brk'
+    AND opmerking LIKE 'GDS2 download van%'
+    AND bestand_naam_hersteld IS NULL;
+
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.0.0_naar_2.0.1','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
 -- versienummer update
