fcfe0cd9e27d6814509a630a733b228e6daaf955
Mark Prins <markprins@b3partners.nl>
2019-12-27 15:26:59 +0100

    Update db-prepare-rsgb.sh

--- .jenkins/db-prepare-rsgb.sh
+++ .jenkins/db-prepare-rsgb.sh
@@ -9,5 +9,5 @@ sqlplus -l -S jenkins_rsgb/jenkins_rsgb@192.168.1.11:1521/ORCL < ./datamodel/gen
 # sqlplus -l -S jenkins_rsgb/jenkins_rsgb@192.168.1.11:1521/ORCL < ./datamodel/utility_scripts/oracle/113a_update_wijk_geom.sql
 
 # update geotools metadata
-sqlplus -l -S jenkins_rsgbbgt/jenkins_rsgbbgt@192.168.1.11:1521/ORCL <<< "update GEOMETRY_COLUMNS set F_TABLE_SCHEMA = 'JENKINS_RSGB';"
-sqlplus -l -S jenkins_rsgbbgt/jenkins_rsgbbgt@192.168.1.11:1521/ORCL <<< "update GT_PK_METADATA set TABLE_SCHEMA = 'JENKINS_RSGB';"
+# sqlplus -l -S jenkins_rsgb/jenkins_rsgb@192.168.1.11:1521/ORCL <<< "update GEOMETRY_COLUMNS set F_TABLE_SCHEMA = 'JENKINS_RSGB';"
+# sqlplus -l -S jenkins_rsgb/jenkins_rsgb@192.168.1.11:1521/ORCL <<< "update GT_PK_METADATA set TABLE_SCHEMA = 'JENKINS_RSGB';"
