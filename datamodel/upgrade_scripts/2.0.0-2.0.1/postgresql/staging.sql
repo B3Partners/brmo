e78007e0c23410798af3eb819df3971d5607a879
Mark Prins <mark@b3partners.nl>
2020-01-02 16:20:43 +0100

    voeg de status STAGING_MISSING toe aan LaadProces
    en maak een index voor laadproces soort

--- datamodel/upgrade_scripts/2.0.0-2.0.1/postgresql/staging.sql
+++ datamodel/upgrade_scripts/2.0.0-2.0.1/postgresql/staging.sql
@@ -15,6 +15,9 @@ WHERE
     AND opmerking LIKE 'GDS2 download van%'
     AND bestand_naam_hersteld IS NULL;
 
+-- # 771 toevoegen STAGING_MISSING status
+CREATE INDEX idx_laadproces_soort ON laadproces(soort);
+
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.0.0_naar_2.0.1','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
 -- versienummer update
