1c4ee2ce7f3f252d43fd2eaf3dcf714e405e9a9d
Mark Prins <mark@b3partners.nl>
2020-02-10 11:18:40 +0100

    GEM-WPL-RELATIE re-gen voor feb 2020

--- datamodel/utility_scripts/sqlserver/201_update_wnplts_gemcode.sql
+++ datamodel/utility_scripts/sqlserver/201_update_wnplts_gemcode.sql
@@ -1,6 +1,6 @@
 -- Update gemeente/woonplaats relatie koppelscript
--- Gegenereerd op: 2019-08-09T08:59:54.179+02:00
--- StandTechnischeDatum van bronbestand: 20190808
+-- Gegenereerd op: 2020-02-10T11:16:25.8+01:00
+-- StandTechnischeDatum van bronbestand: 20200208
 UPDATE wnplts SET fk_7gem_code = 3 WHERE identif = '3386';
 UPDATE wnplts SET fk_7gem_code = 10 WHERE identif = '3232';
 UPDATE wnplts SET fk_7gem_code = 10 WHERE identif = '3233';
