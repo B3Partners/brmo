86980c3e9e8cc1b0a318b802f80efa0cbf065bd0
Mark Prins <mark@b3partners.nl>
2020-02-21 15:04:34 +0100

    Migratie bestanden aanmaken voor upgrade 2.0.1-2.0.2

---
+++ datamodel/upgrade_scripts/2.0.1-2.0.2/postgresql/staging.sql
@@ -0,0 +1,9 @@
+-- 
+-- upgrade PostgreSQL STAGING datamodel van 2.0.1 naar 2.0.2 
+--
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.0.1_naar_2.0.2','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='2.0.2' WHERE naam='brmoversie';
