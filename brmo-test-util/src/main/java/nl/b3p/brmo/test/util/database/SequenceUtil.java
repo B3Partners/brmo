26b1484e8c8033ce7cd9547789376134b2f6e0d4
Mark Prins <mark@b3partners.nl>
2020-05-15 14:05:29 +0200

    Bump jdbc-util 5.0 ➜ 5.1
    close #849

--- brmo-test-util/src/main/java/nl/b3p/brmo/test/util/database/SequenceUtil.java
+++ brmo-test-util/src/main/java/nl/b3p/brmo/test/util/database/SequenceUtil.java
@@ -22,8 +22,10 @@ public class SequenceUtil {
             conn.setAutoCommit(true);
             final GeometryJdbcConverter geomToJdbc = GeometryJdbcConverterFactory.getGeometryJdbcConverter(conn);
             LOG.debug("update sequence: " + sequenceName);
-            // "update" gebruiken omdat we bij oracle stored procedure benaderen
-            Object o = new QueryRunner(geomToJdbc.isPmdKnownBroken()).update(conn, geomToJdbc.getUpdateSequenceSQL(sequenceName, nextVal));
+            // "update" gebruiken omdat we bij oracle stored procedure benaderen?? hoeft niet meer sinds 18c
+            // Object o = new QueryRunner(geomToJdbc.isPmdKnownBroken()).update(conn, geomToJdbc.getUpdateSequenceSQL(sequenceName, nextVal));
+            int rowsUpdated = new QueryRunner(geomToJdbc.isPmdKnownBroken()).execute(conn, geomToJdbc.getUpdateSequenceSQL(sequenceName, nextVal));
+            LOG.trace("updated sequence: " + sequenceName + ", " + rowsUpdated + " rows updated met SQL: " + geomToJdbc.getUpdateSequenceSQL(sequenceName, nextVal));
         }
     }
 }
