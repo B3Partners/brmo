bb72c1e2610a2a872d39921c29fb26e3fc00c201
Mark Prins <mark@b3partners.nl>
2021-02-09 15:54:10 +0100

    valideer alle andere web.xml

---
+++ brmo-stufbg204/src/test/java/nl/b3p/brmo/stufbg204/WebXmlTest.java
@@ -0,0 +1,16 @@
+package nl.b3p.brmo.stufbg204;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static nl.b3p.brmo.test.util.WebXmlTest.testWebXmlIsValidSchemaJavaEE6;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class WebXmlTest {
+    @Test
+    public void validateWebXml() throws Exception {
+        File webxml = new File("src/main/webapp/WEB-INF/web.xml");
+        assertTrue(testWebXmlIsValidSchemaJavaEE6(webxml), "`web.xml` is niet geldig");
+    }
+}
