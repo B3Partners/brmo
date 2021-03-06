55e17cc3f75984928556c8d8d00ebb20cae7a2b2
Mark Prins <mark@b3partners.nl>
2021-03-06 13:37:39 +0100

    Migratie bestanden aanmaken voor upgrade 2.0.3-2.0.4

---
+++ datamodel/upgrade_scripts/2.0.3-2.0.4/sqlserver/staging.sql
@@ -0,0 +1,9 @@
+-- 
+-- upgrade SQLserver STAGING datamodel van 2.0.3 naar 2.0.4 
+--
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.0.3_naar_2.0.4','vorige versie was ' + waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='2.0.4' WHERE naam='brmoversie';
