9c2a74c96e20868b28b9539b745b9d26737d5181
Mark Prins <mark@b3partners.nl>
2021-05-11 17:57:44 +0200

    tweak jenkins oracle login

--- .jenkins/login.sql
+++ .jenkins/login.sql
@@ -4,3 +4,5 @@ SET DEFINE OFF;
 
 -- ALTER system SET nls_length_semantics=CHAR scope=both;
 ALTER session SET nls_length_semantics=CHAR;
+ALTER system SET pga_aggregate_limit=0 scope=both;
+SET AUTOCOMMIT 100;
