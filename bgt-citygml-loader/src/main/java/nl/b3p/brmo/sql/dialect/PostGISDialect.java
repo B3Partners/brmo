d1a56fc4b93d65a6744609711ff5b05eede38e0f
Matthijs Laan <matthijsln@gmail.com>
2021-06-14 18:18:23 +0200

    Do not use LINEARRING for linearized WKT

--- bgt-citygml-loader/src/main/java/nl/b3p/brmo/sql/dialect/PostGISDialect.java
+++ bgt-citygml-loader/src/main/java/nl/b3p/brmo/sql/dialect/PostGISDialect.java
@@ -1,5 +1,6 @@
 package nl.b3p.brmo.sql.dialect;
 
+import nl.b3p.brmo.util.StandardLinearizedWKTWriter;
 import org.geotools.geometry.jts.WKTWriter2;
 import org.locationtech.jts.geom.Geometry;
 import org.postgresql.util.PGobject;
@@ -7,6 +8,7 @@ import org.postgresql.util.PGobject;
 import java.sql.SQLException;
 
 public class PostGISDialect implements SQLDialect{
+    private final StandardLinearizedWKTWriter wktWriter = new StandardLinearizedWKTWriter();
     private final WKTWriter2 wktWriter2 = new WKTWriter2();
 
     @Override
@@ -14,7 +16,7 @@ public class PostGISDialect implements SQLDialect{
         if(geometry == null) {
             return null;
         } else {
-            final String wkt = linearizeCurves ? geometry.toString() : wktWriter2.write(geometry);
+            String wkt = linearizeCurves ? wktWriter.write(geometry) : wktWriter2.write(geometry);
             PGobject object = new PGobject();
             object.setType("geometry");
             object.setValue("SRID=" + geometry.getSRID() + ";" + wkt);
