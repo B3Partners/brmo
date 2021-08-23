873e6414276793ec333297b5e40af0f4d2872fb1
Mark Prins <mark@b3partners.nl>
2021-08-23 15:01:38 +0200

    BRMO-55 Inbouwen BGT loader in brmo-service
    met een paar kleine refactorings in de bgt-loader om het mogelijk te maken een JNDI datasoure te gebruiken ipv url+username+password.

---
+++ brmo-persistence/src/main/java/nl/b3p/brmo/persistence/staging/BGTLoaderProces.java
@@ -0,0 +1,12 @@
+package nl.b3p.brmo.persistence.staging;
+
+import javax.persistence.Entity;
+
+/**
+ * Proces configuratie voor de BGT loader.
+ *
+ * @author mprins
+ */
+@Entity
+public class BGTLoaderProces extends AutomatischProces {
+}
