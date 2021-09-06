63edfcc15ba290b22fd6d0dccfc44c1965892110
Mark Prins <mark@b3partners.nl>
2021-09-06 16:34:13 +0200

    lokale db conn property files voor GH action

---
+++ .build/ci/oracle-setup-db-properties.sh
@@ -0,0 +1,7 @@
+cp -f .build/ci/oracle/github.oracle.brmo-loader.properties ./brmo-loader/src/test/resources/local.oracle.properties
+cp -f .build/ci/oracle/github.oracle.brmo-service.properties ./brmo-service/src/test/resources/local.oracle.properties
+cp -f .build/ci/oracle/github.connections.brmo-persistence.properties ./brmo-persistence/src/test/resources/local.connections.properties
+cp -f .build/ci/oracle/github.oracle.brmo-soap.properties ./brmo-soap/src/test/resources/local.oracle.properties
+cp -f .build/ci/oracle/github.oracle.brmo-stufbg204.properties ./brmo-stufbg204/src/test/resources/local.oracle.properties
+cp -f .build/ci/oracle/github.oracle.datamodel.properties ./datamodel/src/test/resources/local.oracle.properties
+cp -f .build/ci/oracle/github.oracle.brmo-commandline.properties ./brmo-commandline/src/test/resources/oracle.properties
