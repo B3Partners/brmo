f4f79e5aaba77e9b2ed243c6016b41edacf97153
Mark Prins <mark@b3partners.nl>
2021-11-08 17:14:48 +0100

    gebruik gvenzl/oracle-xe:18.4.0-full ipv oracle-xe:full
    want de :full image is upgraded naar 21c geupgraded

--- .build/ci/oracle-start-docker.sh
+++ .build/ci/oracle-start-docker.sh
@@ -2,11 +2,11 @@
 docker version
 
 # this docker image has the following users/credentials (user/password = system/oracle)
-docker pull gvenzl/oracle-xe:full
+docker pull gvenzl/oracle-xe:18.4.0-full
 
 # start the dockerized oracle-xe instance (the container will be destroyed/removed on stopping)
 # this container can be stopped using: docker stop oracle_brmo
-docker run --rm -p 1521:1521 --name oracle_brmo -h oracle_brmo -e ORACLE_PASSWORD=oracle -d gvenzl/oracle-xe:full
+docker run --rm -p 1521:1521 --name oracle_brmo -h oracle_brmo -e ORACLE_PASSWORD=oracle -d gvenzl/oracle-xe:18.4.0-full
 
 printf "\n\nStarting Oracle XE container, this could take a few minutes..."
 printf "\nWaiting for Oracle XE database to start up.... "
