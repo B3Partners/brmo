1b103c96dbf7e4bc9aa3ff59b58f8a46206077a3
Matthijs Laan <matthijsln@gmail.com>
2021-11-23 18:31:58 +0100

    Download afgiftes not downloaded before

--- bag2-loader/src/main/java/nl/b3p/brmo/bag2/loader/cli/BAG2MutatiesCommand.java
+++ bag2-loader/src/main/java/nl/b3p/brmo/bag2/loader/cli/BAG2MutatiesCommand.java
@@ -7,28 +7,41 @@
 
 package nl.b3p.brmo.bag2.loader.cli;
 
+import nl.b3p.brmo.util.ResumingInputStream;
+import nl.b3p.brmo.util.http.HttpStartRangeInputStreamProvider;
+import nl.b3p.brmo.util.http.wrapper.Java11HttpClientWrapper;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import picocli.CommandLine.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.ExitCode;
+import picocli.CommandLine.Option;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.CookieHandler;
+import java.io.OutputStream;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
-import java.net.CookieStore;
-import java.net.HttpCookie;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+import static nl.b3p.brmo.bgt.loader.Utils.formatTimeSince;
+import static org.apache.commons.io.FileUtils.byteCountToDisplaySize;
 
 @Command(name = "mutaties", mixinStandardHelpOptions = true)
 public class BAG2MutatiesCommand {
@@ -37,16 +50,19 @@ public class BAG2MutatiesCommand {
     private static final String KADASTER_LOGIN_URL = "https://mijn.kadaster.nl/security/login.do";
 
     // Artikelnummer 2529 is dagmutaties (https://www.kadaster.nl/-/handleiding-soap-service-bag-2.0-extract)
-    private static final String LVBAG_BESTANDEN_API_URL = "https://bag.kadaster.nl/lvbag/bag-bestanden/api/bestanden?artikelnummers=&gemeenteCodes=";
+    private static final String LVBAG_BESTANDEN_API_URL = "https://bag.kadaster.nl/lvbag/bag-bestanden/api/bestanden?";
 
     @Command(name="download", sortOptions = false)
     public int download(
             @Option(names="--no-delete", negatable = true) boolean noDelete,
             @Option(names="--kadaster-user") String kadasterUser,
             @Option(names="--kadaster-password") String kadasterPassword,
-            @Option(names="--url", defaultValue = LVBAG_BESTANDEN_API_URL) String url
+            @Option(names="--url", defaultValue = LVBAG_BESTANDEN_API_URL) String url,
+            @Option(names="--query-params", defaultValue = "artikelnummers=2529") String queryParams,
+            @Option(names="--path", defaultValue = "") String downloadPath
             ) throws IOException, InterruptedException {
-        System.out.printf("mutaties download, nodelete=%s, kad user=%s, kad pass=%s, url=%s\n", noDelete, kadasterUser, kadasterPassword, url);
+
+        Instant start = Instant.now();
 
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(KADASTER_LOGIN_URL))
                 .header("Content-Type", "application/x-www-form-urlencoded")
@@ -76,12 +92,54 @@ public class BAG2MutatiesCommand {
             log.error("Did not receive KadasterTicketId cookie in response");
             return ExitCode.SOFTWARE;
         }
-        log.info("Auth cookie: " + kadasterTicketIdCookie.get());
 
-        request = HttpRequest.newBuilder().uri(URI.create(LVBAG_BESTANDEN_API_URL + "?artikelnummers")).build();
+        url = url + queryParams;
+        log.info("Opvragen afgiftes vanaf URL " + url);
+        request = HttpRequest.newBuilder().uri(URI.create(url)).build();
         response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.printf("Bestanden response status %d, body: %s\n", response.statusCode(), response.body());
+        JSONArray ja = new JSONArray(response.body());
+
+        log.info("Aantal afgiftes: " + ja.length());
+
+        List<JSONObject> toDownload = new ArrayList<>();
+        for(int i = 0; i < ja.length(); i++) {
+            JSONObject afgifte = ja.getJSONObject(i);
+
+            File f = Path.of(downloadPath, afgifte.getString("naam")).toFile();
+            if (!f.exists() || f.length() != afgifte.getLong("grootte")) {
+                toDownload.add(afgifte);
+            }
+        }
+
+        long totalBytes = toDownload.stream().map(afgifte -> afgifte.getLong("grootte")).reduce(Long::sum).orElse(0L);
+        log.info(String.format("Aantal te downloaden naar directory \"%s\": %d afgiftes (%s)",
+                Path.of("").toAbsolutePath(),
+                toDownload.size(),
+                byteCountToDisplaySize(totalBytes)));
+
+        int count = 0;
+        long bytesRead = 0;
+        for(JSONObject afgifte: toDownload) {
+            url = afgifte.getString("url");
+            String name = afgifte.getString("naam");
+
+            ResumingInputStream input = new ResumingInputStream(new HttpStartRangeInputStreamProvider(URI.create(url),
+                    new Java11HttpClientWrapper(HttpClient.newBuilder().cookieHandler(httpClient.cookieHandler().get()))));
+
+            log.info(String.format("Afgifte %2d/%d (%.1f%%): downloaden %s...",
+                    ++count,
+                    toDownload.size(),
+                    (100.0 / totalBytes) * bytesRead,
+                    name));
+            try(OutputStream out = new FileOutputStream(Path.of(downloadPath, name).toFile())) {
+                IOUtils.copy(input, out);
+            }
+            bytesRead += afgifte.getLong("grootte");
+        }
+        if (!toDownload.isEmpty()) {
+            log.info("Alle afgiftes gedownload in " + formatTimeSince(start));
+        }
 
         return ExitCode.OK;
     }
