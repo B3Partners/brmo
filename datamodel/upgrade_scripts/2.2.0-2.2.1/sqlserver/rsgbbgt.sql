4fa0ff692f39873d6b4f644ae34a1ccd8c08c887
Mark Prins <mark@b3partners.nl>
2021-11-26 17:38:09 +0100

    Migratie bestanden aanmaken voor upgrade 2.2.0-2.2.1

---
+++ datamodel/upgrade_scripts/2.2.0-2.2.1/sqlserver/rsgbbgt.sql
@@ -0,0 +1,9 @@
+-- 
+-- upgrade SQLserver RSGBBGT datamodel van 2.2.0 naar 2.2.1 
+--
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.2.0_naar_2.2.1','vorige versie was ' + waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='2.2.1' WHERE naam='brmoversie';
