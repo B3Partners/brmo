8ca16e4848e46aafe2c7e237053c2fabdf0b9402
Mark Prins <mark@b3partners.nl>
2021-11-29 11:04:38 +0100

    Update upgrade script generator
    maak brmo_metadata aan in bgt schema als die nog niet bestaat

--- datamodel/upgrade_scripts/2.2.0-2.2.1/postgresql/rsgbbgt.sql
+++ datamodel/upgrade_scripts/2.2.0-2.2.1/postgresql/rsgbbgt.sql
@@ -2,6 +2,9 @@
 -- upgrade PostgreSQL RSGBBGT datamodel van 2.2.0 naar 2.2.1 
 --
 
+CREATE TABLE IF NOT EXISTS brmo_metadata(naam CHARACTER VARYING(255) NOT NULL,waarde CHARACTER VARYING(255),CONSTRAINT brmo_metadata_pk PRIMARY KEY (naam));
+INSERT INTO brmo_metadata(naam) VALUES('brmoversie') ON CONFLICT DO NOTHING;
+
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.2.0_naar_2.2.1','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
