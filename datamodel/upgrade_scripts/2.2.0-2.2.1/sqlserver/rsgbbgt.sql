699ee6776f381b9de0f1065171f099f675975d5e
Matthijs Laan <matthijsln@gmail.com>
2022-02-04 14:15:47 +0100

    Change update scripts to modify brmo_metadata.waarde column type to clob for BGT

--- datamodel/upgrade_scripts/2.2.0-2.2.1/sqlserver/rsgbbgt.sql
+++ datamodel/upgrade_scripts/2.2.0-2.2.1/sqlserver/rsgbbgt.sql
@@ -3,8 +3,10 @@
 --
 
 IF OBJECT_ID('brmo_metadata', 'U') IS NULL
-CREATE TABLE brmo_metadata(naam VARCHAR(255) NOT NULL,waarde VARCHAR(255),PRIMARY KEY (naam));
+CREATE TABLE brmo_metadata(naam VARCHAR(255) NOT NULL,waarde NTEXT,PRIMARY KEY (naam));
 GO
+ALTER TABLE brmo_metadata ALTER COLUMN waarde NTEXT;
+
 INSERT INTO brmo_metadata(naam) SELECT naam FROM brmo_metadata WHERE NOT('brmoversie' IN (SELECT naam FROM brmo_metadata));
 
 
