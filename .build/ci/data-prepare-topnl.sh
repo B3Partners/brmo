68b1fd54e3835939fd939fb542374f0179d7b116
Mark Prins <mark@b3partners.nl>
2022-03-14 09:45:39 +0100

    [BRMO-154] Fix Top250 download link

--- .build/ci/data-prepare-topnl.sh
+++ .build/ci/data-prepare-topnl.sh
@@ -5,7 +5,8 @@ set -e
 wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "http://geodata.nationaalgeoregister.nl/top10nlv2/extract/kaartbladen/TOP10NL_07W.zip?formaat=gml" --output-document=./TOP10NL_07W.zip
 wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "http://geodata.nationaalgeoregister.nl/top50nl/extract/kaartbladen/TOP50NL_07W.zip?formaat=gml" --output-document=./TOP50NL_07W.zip
 wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "http://geodata.nationaalgeoregister.nl/top100nl/extract/chunkdata/top100nl_gml_filechunks.zip?formaat=gml" --output-document=./TOP100NL.zip
-wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "http://geodata.nationaalgeoregister.nl/top250nl/extract/kaartbladtotaal/top250nl.zip?formaat=gml" --output-document=./TOP250NL.zip
+wget --no-verbose --tries=5 --timeout=60 --waitretry=300 --user-agent="" "https://api.pdok.nl/brt/top250nl/download/v1_0/cache/24/b45351fd-7fd4-4259-bdb4-76b495b0e335/top250nl-gml-nl-nohist.zip" --output-document=./TOP250NL.zip
+
 # uitpakken in de /tmp dir
 ls *.zip | while read filename; do unzip -j -o -d "/tmp" "$filename" *.gml; done;
 # lowercase alle gml files uit de zips
