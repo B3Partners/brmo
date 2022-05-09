329ee405284ea90e9af4a90a9b0688f98d30de9d
Puck Meerburg <puck@puckipedia.com>
2022-05-09 14:27:05 +0000

    datamodel: add NHR tables to upgrade script

--- datamodel/upgrade_scripts/2.2.2-2.3.0/oracle/staging.sql
+++ datamodel/upgrade_scripts/2.2.2-2.3.0/oracle/staging.sql
@@ -4,6 +4,17 @@
 
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 
+    create table nhr_laadproces (
+        datum timestamp,
+        laatst_geprobeerd timestamp,
+        volgend_proberen timestamp,
+        probeer_aantal number(19, 0),
+        kvk_nummer varchar2(255 char) not null,
+        exception clob,
+        primary key (kvk_nummer)
+    );
+
+create index idx_nhr_laadproces_volgend_proberen on nhr_laadproces(volgend_proberen);
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.2.2_naar_2.3.0','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
