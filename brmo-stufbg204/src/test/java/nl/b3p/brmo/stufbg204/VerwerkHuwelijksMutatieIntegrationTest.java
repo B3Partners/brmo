6e32df8e0c691c46374cf4ab58a84b4f5951285d
Mark Prins <mark@b3partners.nl>
2022-05-12 17:13:46 +0200

    [BRMO-169] opruimen mssql specifieke code uit brmo-stufbg204 module

--- brmo-stufbg204/src/test/java/nl/b3p/brmo/stufbg204/VerwerkHuwelijksMutatieIntegrationTest.java
+++ brmo-stufbg204/src/test/java/nl/b3p/brmo/stufbg204/VerwerkHuwelijksMutatieIntegrationTest.java
@@ -20,7 +20,6 @@ import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.DatabaseDataSourceConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.ITable;
-import org.dbunit.ext.mssql.MsSqlDataTypeFactory;
 import org.dbunit.ext.oracle.Oracle10DataTypeFactory;
 import org.dbunit.ext.postgresql.PostgresqlDataTypeFactory;
 import org.junit.jupiter.api.AfterEach;
@@ -62,10 +61,7 @@ public class VerwerkHuwelijksMutatieIntegrationTest extends WebTestStub {
         staging = new DatabaseDataSourceConnection(dsStaging);
         rsgb = new DatabaseDataSourceConnection(dsRsgb);
 
-        if (this.isMsSQL) {
-            staging.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MsSqlDataTypeFactory());
-            rsgb.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MsSqlDataTypeFactory());
-        } else if (this.isOracle) {
+        if (this.isOracle) {
             dsStaging.getConnection().setAutoCommit(true);
             dsRsgb.getConnection().setAutoCommit(true);
             staging = new DatabaseConnection(OracleConnectionUnwrapper.unwrap(dsStaging.getConnection()), DBPROPS.getProperty("staging.username").toUpperCase());
