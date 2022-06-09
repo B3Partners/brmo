8fcf05a6ea3c3c164262ed259c0a835c0951c28b
Matthijs Laan <matthijsln@gmail.com>
2022-06-09 09:51:16 +0200

    Update scripts for objectid default values

--- datamodel/upgrade_scripts/2.3.0-2.3.1/oracle/bag.sql
+++ datamodel/upgrade_scripts/2.3.0-2.3.1/oracle/bag.sql
@@ -16,6 +16,39 @@ END;
 /
 MERGE INTO brmo_metadata USING DUAL ON (naam = 'brmoversie') WHEN NOT MATCHED THEN INSERT (naam) VALUES('brmoversie');
 
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+BEGIN
+EXECUTE IMMEDIATE 'alter table ligplaats modify (objectid drop identity)';
+EXECUTE IMMEDIATE 'alter table nummeraanduiding modify (objectid drop identity)';
+EXECUTE IMMEDIATE 'alter table openbareruimte modify (objectid drop identity)';
+EXECUTE IMMEDIATE 'alter table pand modify (objectid drop identity)';
+EXECUTE IMMEDIATE 'alter table standplaats modify (objectid drop identity)';
+EXECUTE IMMEDIATE 'alter table verblijfsobject modify (objectid drop identity)';
+EXECUTE IMMEDIATE 'alter table woonplaats modify (objectid drop identity)';
+EXECUTE IMMEDIATE 'alter table ligplaats modify objectid integer default objectid_seq.nextval';
+EXECUTE IMMEDIATE 'alter table nummeraanduiding modify objectid integer default objectid_seq.nextval';
+EXECUTE IMMEDIATE 'alter table openbareruimte modify objectid integer default objectid_seq.nextval';
+EXECUTE IMMEDIATE 'alter table pand modify objectid integer default objectid_seq.nextval';
+EXECUTE IMMEDIATE 'alter table standplaats modify objectid integer default objectid_seq.nextval';
+EXECUTE IMMEDIATE 'alter table verblijfsobject modify objectid integer default objectid_seq.nextval';
+EXECUTE IMMEDIATE 'alter table woonplaats modify objectid integer default objectid_seq.nextval';
+EXECUTE IMMEDIATE 'create sequence objectid_seq';
+EXECUTE IMMEDIATE 'update ligplaats set objectid = objectid_seq.nextval';
+EXECUTE IMMEDIATE 'update nummeraanduiding set objectid = objectid_seq.nextval';
+EXECUTE IMMEDIATE 'update openbareruimte set objectid = objectid_seq.nextval';
+EXECUTE IMMEDIATE 'update pand set objectid = objectid_seq.nextval';
+EXECUTE IMMEDIATE 'update standplaats set objectid = objectid_seq.nextval';
+EXECUTE IMMEDIATE 'update verblijfsobject set objectid = objectid_seq.nextval';
+EXECUTE IMMEDIATE 'update woonplaats set objectid = objectid_seq.nextval';
+EXCEPTION
+WHEN OTHERS THEN
+IF
+    SQLCODE = -942 THEN
+    NULL;
+ELSE RAISE;
+END IF;
+END;
+/
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.3.0_naar_2.3.1','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
--- datamodel/upgrade_scripts/2.3.0-2.3.1/postgresql/bag.sql
+++ datamodel/upgrade_scripts/2.3.0-2.3.1/postgresql/bag.sql
@@ -6,6 +6,28 @@ CREATE SCHEMA IF NOT EXISTS bag;
 
 SET search_path = bag,public;
 
+create sequence objectid_seq;
+alter table ligplaats alter column objectid set default nextval('objectid_seq');
+alter table nummeraanduiding alter column objectid set default nextval('objectid_seq');
+alter table openbareruimte alter column objectid set default nextval('objectid_seq');
+alter table pand alter column objectid set default nextval('objectid_seq');
+alter table standplaats alter column objectid set default nextval('objectid_seq');
+alter table verblijfsobject alter column objectid set default nextval('objectid_seq');
+alter table woonplaats alter column objectid set default nextval('objectid_seq');
+drop sequence ligplaats_objectid_seq;
+drop sequence nummeraanduiding_objectid_seq;
+drop sequence openbareruimte_objectid_seq;
+drop sequence pand_objectid_seq;
+drop sequence standplaats_objectid_seq;
+drop sequence verblijfsobject_objectid_seq;
+drop sequence woonplaats_objectid_seq;
+update ligplaats set objectid = nextval('objectid_seq');
+update nummeraanduiding set objectid = nextval('objectid_seq');
+update openbareruimte set objectid = nextval('objectid_seq');
+update pand set objectid = nextval('objectid_seq');
+update standplaats set objectid = nextval('objectid_seq');
+update verblijfsobject set objectid = nextval('objectid_seq');
+update woonplaats set objectid = nextval('objectid_seq');
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.3.0_naar_2.3.1','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
