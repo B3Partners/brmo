f9f26a980cf469e78b432f6c2938241d6729921d
Mark Prins <1165786+mprins@users.noreply.github.com>
2022-08-29 11:27:38 +0200

    gvenzl/oracle-xe is nu beschikbaar op ghcr.io met potentieel snellere download (#1509)

--- .build/ci/oracle-start-docker.sh
+++ .build/ci/oracle-start-docker.sh
@@ -3,11 +3,11 @@ set -e
 docker version
 
 # this docker image has the following users/credentials (user/password = system/oracle)
-docker pull gvenzl/oracle-xe:$1
+docker pull ghcr.io/gvenzl/oracle-xe:$1
 
 # start the dockerized oracle-xe instance (the container will be destroyed/removed on stopping)
 # this container can be stopped using: docker stop oracle_brmo
-docker run --rm -p 1521:1521 --name oracle_brmo -h oracle_brmo -e ORACLE_PASSWORD=oracle -d gvenzl/oracle-xe:$1
+docker run --rm -p 1521:1521 --name oracle_brmo -h oracle_brmo -e ORACLE_PASSWORD=oracle -d ghcr.io/gvenzl/oracle-xe:$1
 
 printf "\n\nStarting Oracle $1 XE container, this could take a few minutes..."
 printf "\nWaiting for Oracle $1 XE database to start up.... "
