e2bbd8d40c04aa85a6dacf658ba88aa0a9a3420b
Mark Prins <1165786+mprins@users.noreply.github.com>
2022-10-24 19:12:11 +0200

    [BRMO-209] drop constraint fk_locaand_openb_rmte_sc_lh zodat laden van WOZ object met openbare ruimte verwijzing mogelijk is, zelfs als het bag object niet geladen is (#1562)

--- datamodel/upgrade_scripts/2.3.2-2.3.3/oracle/rsgb.sql
+++ datamodel/upgrade_scripts/2.3.2-2.3.3/oracle/rsgb.sql
@@ -4,6 +4,8 @@
 
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 
+-- https://b3partners.atlassian.net/browse/BRMO-209
+ALTER TABLE LOCAAND_OPENB_RMTE DROP CONSTRAINT FK_LOCAAND_OPENB_RMTE_SC_LH;
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.3.2_naar_2.3.3','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
--- datamodel/upgrade_scripts/2.3.2-2.3.3/postgresql/rsgb.sql
+++ datamodel/upgrade_scripts/2.3.2-2.3.3/postgresql/rsgb.sql
@@ -4,6 +4,8 @@
 
 set search_path = public,bag;
 
+-- https://b3partners.atlassian.net/browse/BRMO-209
+alter table locaand_openb_rmte drop constraint fk_locaand_openb_rmte_sc_lh;
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.3.2_naar_2.3.3','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
