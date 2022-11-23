332958633d104f38310866a3f3e242fcc6806fe1
Puck Meerburg <puck@puckipedia.com>
2022-11-23 13:26:04 +0000

    [SUPPORT-13357] Fix 2.3.1->2.3.2 Postgres upgrade syntax

--- datamodel/upgrade_scripts/2.3.1-2.3.2/postgresql/rsgb.sql
+++ datamodel/upgrade_scripts/2.3.1-2.3.2/postgresql/rsgb.sql
@@ -4,8 +4,8 @@
 
 set search_path = public,bag;
 
-ALTER TABLE ingeschr_niet_nat_prs MODIFY publiekrechtelijke_rechtsvorm VARCHAR2(60);
-ALTER TABLE functionaris MODIFY soort_bev VARCHAR2(50);
+ALTER TABLE ingeschr_niet_nat_prs ALTER COLUMN publiekrechtelijke_rechtsvorm TYPE VARCHAR(60);
+ALTER TABLE functionaris ALTER COLUMN soort_bev TYPE VARCHAR(50);
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_2.3.1_naar_2.3.2','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
