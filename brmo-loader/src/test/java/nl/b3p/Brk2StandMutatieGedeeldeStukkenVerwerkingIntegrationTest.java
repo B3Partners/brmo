5257a39c94cc98d9ab83c730efd5c8635fc41f9d
Mark Prins <1165786+mprins@users.noreply.github.com>
2023-01-12 15:09:15 +0100

    skip testcase on github/pgsql

--- brmo-loader/src/test/java/nl/b3p/Brk2StandMutatieGedeeldeStukkenVerwerkingIntegrationTest.java
+++ brmo-loader/src/test/java/nl/b3p/Brk2StandMutatieGedeeldeStukkenVerwerkingIntegrationTest.java
@@ -28,6 +28,7 @@ import org.dbunit.operation.DatabaseOperation;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -57,7 +58,11 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
  * voor Oracle.
  *
  * @author mprins
+ *
+ * @todo Deze test draait niet op github omdat er nogal eens een database fout optreed.
+ *
  */
+@Tag("skip-flakey-pgsql-github")
 class Brk2StandMutatieGedeeldeStukkenVerwerkingIntegrationTest extends AbstractDatabaseIntegrationTest {
 
     private static final Log LOG = LogFactory.getLog(Brk2StandMutatieGedeeldeStukkenVerwerkingIntegrationTest.class);
