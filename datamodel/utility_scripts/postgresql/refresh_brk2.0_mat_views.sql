1c730f6d1aac29254ab89df0cc18db9205d49697
Geert Offenberg <geertoff@gmail.com>
2023-12-14 14:45:36 +0100

    verversvolgorde brk2.0/bag2.0 mat-views

---
+++ datamodel/utility_scripts/postgresql/refresh_brk2.0_mat_views.sql
@@ -0,0 +1,13 @@
+-- Volgorde van het verversen van de BRK 2.0 en BRK 2.0 x BAG 2.0 materialized views  
+REFRESH MATERIALIZED VIEW brk.mb_percelenkaart with data;
+REFRESH MATERIALIZED VIEW brk.mb_subject with data;
+REFRESH MATERIALIZED VIEW brk.mb_avg_subject with data;
+REFRESH MATERIALIZED VIEW brk.mb_kad_onrrnd_zk_adres with data;
+REFRESH MATERIALIZED VIEW brk.mb_kad_onrrnd_zk_archief with data;
+REFRESH MATERIALIZED VIEW brk.mb_zr_rechth with data;
+REFRESH MATERIALIZED VIEW brk.mb_avg_zr_rechth with data;
+REFRESH MATERIALIZED VIEW brk.mb_koz_rechth with data;
+REFRESH MATERIALIZED VIEW brk.mb_avg_koz_rechth with data;
+REFRESH MATERIALIZED VIEW public.mb_kadastraleonroerendezakenmetadres with data;
+REFRESH MATERIALIZED VIEW public.mb_onroerendezakenmetrechthebbenden with data;
+REFRESH MATERIALIZED VIEW public.mb_avg_onroerendezakenmetrechthebbenden with data;
