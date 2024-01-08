ede18e9babd6a954ee29d23049664064bf7fdf74
Geert Offenberg <geertoff@gmail.com>
2024-01-08 14:24:46 +0100

    toevoegen van brk & bag 2.0 materialized views in setup

--- .build/ci/oracle-setup-bag2_views.sh
+++ .build/ci/oracle-setup-bag2_views.sh
@@ -7,3 +7,5 @@ printf "\nSetup BAG grants...\n"
 docker exec -i oracle_brmo sqlplus -l jenkins_bag/jenkins_bag@//localhost:1521/XE < ./.build/ci/oracle-db-grant-bag-to-rsgb.sql
 printf "\nset up RSGB BAG views...\n"
 docker exec -i oracle_brmo sqlplus -l jenkins_rsgb/jenkins_rsgb@//localhost:1521/XE < ./datamodel/extra_scripts/oracle/209_bag2_rsgb_views.sql
+printf "\nset up RSGB BAG and BRK materialized views...\n"
+docker exec -i oracle_brmo sqlplus -l jenkins_rsgb/jenkins_rsgb@//localhost:1521/XE < ./datamodel/extra_scripts/oracle/210_bag2_brk2.0_mat_views.sql
--- .build/ci/pgsql-setup-bag2_views.sh
+++ .build/ci/pgsql-setup-bag2_views.sh
@@ -5,3 +5,5 @@ printf "\nset up BAG views...\n"
 psql -U postgres -h localhost -w -d rsgb -f ./datamodel/extra_scripts/postgresql/208_bag2_views.sql
 printf "\nset up RSGB BAG views...\n"
 psql -U postgres -h localhost -w -d rsgb -f ./datamodel/extra_scripts/postgresql/209_bag2_rsgb_views.sql
+printf "\nset up RSGB BAG and BRK materialized views...\n"
+psql -U postgres -h localhost -w -d rsgb -f ./datamodel/extra_scripts/postgresql/210_bag2_brk2.0_mat_views.sql
