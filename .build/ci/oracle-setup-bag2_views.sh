eb588fac710a41ad68bb4ef66f195e4651bda1ca
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-01-25 15:37:24 +0100

    Update oracle-setup-bag2_views.sh

--- .build/ci/oracle-setup-bag2_views.sh
+++ .build/ci/oracle-setup-bag2_views.sh
@@ -8,4 +8,5 @@ docker exec -i oracle_brmo sqlplus -l jenkins_bag/jenkins_bag@//localhost:1521/X
 printf "\nset up RSGB BAG views...\n"
 docker exec -i oracle_brmo sqlplus -l jenkins_rsgb/jenkins_rsgb@//localhost:1521/XE < ./datamodel/extra_scripts/oracle/209_bag2_rsgb_views.sql
 printf "\nset up RSGB BAG and BRK materialized views...\n"
+sed -i 's/BRMO_BRK/JENKINS_BRK/g' ./datamodel/extra_scripts/oracle/210_bag2_brk2.0_mat_views.sql
 docker exec -i oracle_brmo sqlplus -l jenkins_rsgb/jenkins_rsgb@//localhost:1521/XE < ./datamodel/extra_scripts/oracle/210_bag2_brk2.0_mat_views.sql
