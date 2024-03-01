3f5d92d952b8c2767f9bdc4670c5f59134b49610
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-03-01 13:24:29 +0100

    [BRMO-123] Verwijder BAGScannerProces en WebMirrorBAGScannerProces uit de configuratie via upgrade procedure

--- datamodel/upgrade_scripts/3.0.2-4.0.0/postgresql/staging.sql
+++ datamodel/upgrade_scripts/3.0.2-4.0.0/postgresql/staging.sql
@@ -3,6 +3,10 @@
 --
 
 
+-- opruimen van niet langer beschikbare automatische processen
+DELETE FROM automatisch_proces_config WHERE proces_id IN (SELECT id FROM automatisch_proces WHERE dtype IN ('BAGScannerProces','WebMirrorBAGScannerProces')) ;
+DELETE FROM automatisch_proces WHERE dtype IN ('BAGScannerProces','WebMirrorBAGScannerProces');
+
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_3.0.2_naar_4.0.0','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
 -- versienummer update
