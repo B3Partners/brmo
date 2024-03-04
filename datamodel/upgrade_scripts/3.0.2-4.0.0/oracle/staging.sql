832e0c2992d69da354977d513fa0649db9bcfae0
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-03-04 14:24:27 +0100

    fix oracle 4.0.0 staging upgrade

--- datamodel/upgrade_scripts/3.0.2-4.0.0/oracle/staging.sql
+++ datamodel/upgrade_scripts/3.0.2-4.0.0/oracle/staging.sql
@@ -8,14 +8,11 @@ WHENEVER SQLERROR EXIT SQL.SQLCODE
 DELETE FROM automatisch_proces_config WHERE proces_id IN (SELECT id FROM automatisch_proces WHERE dtype IN ('BAGScannerProces','WebMirrorBAGScannerProces')) ;
 DELETE FROM automatisch_proces WHERE dtype IN ('BAGScannerProces','WebMirrorBAGScannerProces');
 
-BEGIN
-    -- BAG1 GDS2 processen verwijderen
-    EXECUTE IMMEDIATE 'CREATE PRIVATE TEMPORARY TABLE ora$ptt_pids AS (SELECT apc.proces_id FROM automatisch_proces_config apc WHERE to_char(apc.value) = ''bag'' AND apc.config_key=''gds2_br_soort'')';
-    EXECUTE IMMEDIATE 'SELECT * FROM ora$ptt_pids';
-    EXECUTE IMMEDIATE 'DELETE FROM automatisch_proces_config WHERE proces_id IN (SELECT proces_id FROM ora$ptt_pids)';
-    EXECUTE IMMEDIATE 'DELETE FROM automatisch_proces WHERE id IN (SELECT proces_id FROM ora$ptt_pids)';
-    EXECUTE IMMEDIATE 'DROP TABLE ora$ptt_pids';
-END;
+CREATE TABLE tmp_pids AS (SELECT apc.proces_id FROM automatisch_proces_config apc WHERE to_char(apc.value)='bag' AND apc.config_key='gds2_br_soort');
+DELETE FROM automatisch_proces_config WHERE proces_id IN (SELECT proces_id FROM tmp_pids);
+DELETE FROM automatisch_proces WHERE id IN (SELECT proces_id FROM tmp_pids);
+DROP TABLE tmp_pids;
+
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_3.0.2_naar_4.0.0','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
