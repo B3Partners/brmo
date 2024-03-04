8a9ebc37f500a92e2be719e9cf262ccbee5892dd
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-03-04 10:59:12 +0100

    [BRMO-123] Verwijder BAG GDS2 processen uit configuratie via upgrade procedure

--- datamodel/upgrade_scripts/3.0.2-4.0.0/oracle/staging.sql
+++ datamodel/upgrade_scripts/3.0.2-4.0.0/oracle/staging.sql
@@ -4,10 +4,18 @@
 
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 
--- opruimen van niet langer beschikbare automatische processen
+-- opruimen van niet langer beschikbare automatische scanner processen
 DELETE FROM automatisch_proces_config WHERE proces_id IN (SELECT id FROM automatisch_proces WHERE dtype IN ('BAGScannerProces','WebMirrorBAGScannerProces')) ;
 DELETE FROM automatisch_proces WHERE dtype IN ('BAGScannerProces','WebMirrorBAGScannerProces');
 
+BEGIN
+    -- BAG1 GDS2 processen verwijderen
+    EXECUTE IMMEDIATE 'CREATE PRIVATE TEMPORARY TABLE ora$ptt_pids AS (SELECT apc.proces_id FROM automatisch_proces_config apc WHERE to_char(apc.value) = ''bag'' AND apc.config_key=''gds2_br_soort'')';
+    EXECUTE IMMEDIATE 'SELECT * FROM ora$ptt_pids';
+    EXECUTE IMMEDIATE 'DELETE FROM automatisch_proces_config WHERE proces_id IN (SELECT proces_id FROM ora$ptt_pids)';
+    EXECUTE IMMEDIATE 'DELETE FROM automatisch_proces WHERE id IN (SELECT proces_id FROM ora$ptt_pids)';
+    EXECUTE IMMEDIATE 'DROP TABLE ora$ptt_pids';
+END;
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_3.0.2_naar_4.0.0','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
