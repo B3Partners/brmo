ee81903133015d5120634276785fe8333955923b
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-03-12 14:34:22 +0100

    [BRMO-363] gebruik VARCHAR2(5) voor boolean velden in topnl Oracle tabellen

--- .build/ci/oracle-execute-upgrade-extras.sh
+++ .build/ci/oracle-execute-upgrade-extras.sh
@@ -18,3 +18,8 @@ if [ $CURSNAPSHOT = "4.0.0-SNAPSHOT" ] && [ $1 = "staging" ]
 then
     docker exec -i oracle_brmo sqlplus -l -S jenkins_$1/jenkins_$1@//localhost:1521/FREE < ./datamodel/utility_scripts/oracle/1000_delete_bag_berichten.sql
 fi
+
+if [ $CURSNAPSHOT = "4.0.0-SNAPSHOT" ] && [ $1 = "topnl" ]
+then
+    docker exec -i oracle_brmo sqlplus -l -S sys/oracle@//localhost:1521/XE < ./datamodel/upgrade_scripts/3.0.2-4.0.0/oracle/topnl.sql
+fi
