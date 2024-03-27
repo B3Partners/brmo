0045942a836ce00c45f361893b2c410e007db5a2
Geert Offenberg <geertoff@gmail.com>
2024-03-27 11:23:51 +0100

    oracle - toevoegen van hoofd-nevenvestiging view

---
+++ datamodel/extra_scripts/oracle/211_nhr_views.sql
@@ -0,0 +1,11 @@
+create or replace view v_kvk_hoofd_nevenvestiging as 
+select      s.naam as hoofdvestigingnaam,
+            coalesce(v.fk_15ond_kvk_nummer, v.fk_17mac_kvk_nummer)                                 as kvknummer,
+            listagg(distinct s2.naam, ' | ' on overflow truncate) within group ( order by s2.naam) as nevenvestigingen,
+            count(v2.sc_identif)                                                                   as aantal_nevenvestigingen
+from vestg v
+join subject s on v.sc_identif = s.identif
+join vestg v2 on coalesce(v.fk_15ond_kvk_nummer, v.fk_17mac_kvk_nummer) = coalesce(v2.fk_15ond_kvk_nummer, v2.fk_17mac_kvk_nummer)
+join subject s2 on v2.sc_identif = s2.identif
+where v.hoofdvestiging = 'Ja' and v2.hoofdvestiging = 'Nee'
+group by s.naam, coalesce(v.fk_15ond_kvk_nummer, v.fk_17mac_kvk_nummer);
