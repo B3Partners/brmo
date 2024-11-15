9b50d02d051ed83189afae0836248c6be0ff30d5
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-11-15 15:30:27 +0100

    Migratie bestanden aanmaken voor upgrade 4.0.0-4.0.1

---
+++ datamodel/upgrade_scripts/4.0.0-4.0.1/oracle/rsgbbgt.sql
@@ -0,0 +1,23 @@
+-- 
+-- upgrade Oracle RSGBBGT datamodel van 4.0.0 naar 4.0.1 
+--
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+BEGIN
+    EXECUTE IMMEDIATE 'CREATE TABLE brmo_metadata(naam VARCHAR2(255 CHAR) NOT NULL, waarde CLOB, PRIMARY KEY (naam))';
+EXCEPTION
+WHEN OTHERS THEN
+IF
+    SQLCODE = -955 THEN
+    NULL;
+ELSE RAISE;
+END IF;
+END;
+/
+MERGE INTO brmo_metadata USING DUAL ON (naam = 'brmoversie') WHEN NOT MATCHED THEN INSERT (naam) VALUES('brmoversie');
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_4.0.0_naar_4.0.1','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='4.0.1' WHERE naam='brmoversie';
---
+++ datamodel/upgrade_scripts/4.0.0-4.0.1/postgresql/bag.sql
@@ -0,0 +1,13 @@
+-- 
+-- upgrade PostgreSQL BAG datamodel van 4.0.0 naar 4.0.1 
+--
+
+CREATE SCHEMA IF NOT EXISTS bag;
+
+SET search_path = bag,public;
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_4.0.0_naar_4.0.1','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='4.0.1' WHERE naam='brmoversie';
