f6bf7bda7039f4b635b1b63903f722d68833f8fb
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-12-10 16:01:18 +0100

    Update truncate scripts voor opruimen BRK 1 data, update dist

--- datamodel/utility_scripts/oracle/204_truncate_brk_tables.sql
+++ datamodel/utility_scripts/oracle/204_truncate_brk_tables.sql
@@ -1,41 +1,40 @@
 --Pas op, zorg dat constraints tussen BRK en andere
 --BR gedropt zijn, anders gaat de gehele database leeg.
 
--- kadastrale objecten
--- TODO mogelijk is onderstaande volgorde niet helemaal correct en oracle < 12.1 heeft geen cascade optie!
-TRUNCATE TABLE app_re_kad_perceel CASCADE;
-TRUNCATE TABLE app_re_kad_perceel_archief CASCADE;
-TRUNCATE TABLE zak_recht_aantek CASCADE;
-TRUNCATE TABLE zak_recht CASCADE;
-TRUNCATE TABLE app_re_archief CASCADE;
-TRUNCATE TABLE app_re CASCADE;
-TRUNCATE TABLE kad_perceel_archief CASCADE;
-TRUNCATE TABLE kad_perceel CASCADE;
-TRUNCATE TABLE kad_onrrnd_zk_kad_onrr_archief CASCADE;
-TRUNCATE TABLE kad_onrrnd_zk_kad_onrrnd_zk CASCADE;
-TRUNCATE TABLE kad_onrrnd_zk_aantek_archief CASCADE;
-TRUNCATE TABLE kad_onrrnd_zk_archief CASCADE;
-TRUNCATE TABLE kad_onrrnd_zk_aantek CASCADE;
-TRUNCATE TABLE zak_recht_aantek CASCADE;
-TRUNCATE TABLE kad_onrrnd_zk_his_rel CASCADE;
-TRUNCATE TABLE kad_onrrnd_zk CASCADE;
-TRUNCATE TABLE kad_onrrnd_zk_archief CASCADE;
+-- kadastrale objecten: verwijderd in versie 5.0.0
+-- TRUNCATE TABLE app_re_kad_perceel CASCADE;
+-- TRUNCATE TABLE app_re_kad_perceel_archief CASCADE;
+-- TRUNCATE TABLE zak_recht_aantek CASCADE;
+-- TRUNCATE TABLE zak_recht CASCADE;
+-- TRUNCATE TABLE app_re_archief CASCADE;
+-- TRUNCATE TABLE app_re CASCADE;
+-- TRUNCATE TABLE kad_perceel_archief CASCADE;
+-- TRUNCATE TABLE kad_perceel CASCADE;
+-- TRUNCATE TABLE kad_onrrnd_zk_kad_onrr_archief CASCADE;
+-- TRUNCATE TABLE kad_onrrnd_zk_kad_onrrnd_zk CASCADE;
+-- TRUNCATE TABLE kad_onrrnd_zk_aantek_archief CASCADE;
+-- TRUNCATE TABLE kad_onrrnd_zk_archief CASCADE;
+-- TRUNCATE TABLE kad_onrrnd_zk_aantek CASCADE;
+-- TRUNCATE TABLE zak_recht_aantek CASCADE;
+-- TRUNCATE TABLE kad_onrrnd_zk_his_rel CASCADE;
+-- TRUNCATE TABLE kad_onrrnd_zk CASCADE;
+-- TRUNCATE TABLE kad_onrrnd_zk_archief CASCADE;
 
 --
 -- personen/subjecten ed. die mogelijk uit kadaster komen,
--- mogelijk uit andere BR met personen (NHR, BRP)
+-- mogelijk uit andere BR met personen (NHR, BRP, WOZ)
 -- controleer eerst met:
 --        SELECT DISTINCT herkomst_br FROM herkomst_metadata CASCADE;
--- truncate worden geleegd omdat dan er meer dan BRK informatie verloren gaat
--- truncate worden geleegd omdat dan er meer dan BRK informatie verloren gaat
+-- als daar meer dan 1 type BR uit komt kunnen tabellen niet met
+-- truncate worden geleegd omdat dan er meer dan alleen BRK informatie verloren gaat
 --
---TRUNCATE TABLE herkomst_metadata CASCADE;
---TRUNCATE TABLE ander_btnlnds_niet_nat_prs CASCADE;
---TRUNCATE TABLE ingeschr_niet_nat_prs CASCADE;
---TRUNCATE TABLE niet_nat_prs CASCADE;
---TRUNCATE TABLE ander_nat_prs CASCADE;
---TRUNCATE TABLE niet_ingezetene CASCADE;
---TRUNCATE TABLE ingeschr_nat_prs CASCADE;
---TRUNCATE TABLE nat_prs CASCADE;
---TRUNCATE TABLE prs CASCADE;
---TRUNCATE TABLE subject CASCADE;
+TRUNCATE TABLE herkomst_metadata CASCADE;
+TRUNCATE TABLE ander_btnlnds_niet_nat_prs CASCADE;
+TRUNCATE TABLE ingeschr_niet_nat_prs CASCADE;
+TRUNCATE TABLE niet_nat_prs CASCADE;
+TRUNCATE TABLE ander_nat_prs CASCADE;
+TRUNCATE TABLE niet_ingezetene CASCADE;
+TRUNCATE TABLE ingeschr_nat_prs CASCADE;
+TRUNCATE TABLE nat_prs CASCADE;
+TRUNCATE TABLE prs CASCADE;
+TRUNCATE TABLE subject CASCADE;
--- datamodel/utility_scripts/postgresql/204_truncate_brk_tables.sql
+++ datamodel/utility_scripts/postgresql/204_truncate_brk_tables.sql
@@ -1,40 +1,40 @@
 --Pas op, zorg dat constraints tussen BRK en andere
 --BR gedropt zijn, anders gaat de gehele database leeg.
 
--- kadastrale objecten
-truncate app_re cascade;
-truncate app_re_archief cascade;
-truncate app_re_kad_perceel cascade;
-truncate app_re_kad_perceel_archief cascade;
-truncate kad_onrrnd_zk cascade;
-truncate kad_onrrnd_zk_aantek cascade;
-truncate kad_onrrnd_zk_aantek_archief cascade;
-truncate kad_onrrnd_zk_archief cascade;
-truncate kad_onrrnd_zk_his_rel cascade;
-truncate kad_onrrnd_zk_kad_onrr_archief cascade;
-truncate kad_onrrnd_zk_kad_onrrnd_zk cascade;
-truncate kad_perceel cascade;
-truncate kad_perceel_archief cascade;
-truncate zak_recht_archief cascade;
-truncate zak_recht cascade;
-truncate zak_recht_aantek cascade;
-truncate zak_recht_archief cascade;
+-- kadastrale objecten: verwijderd in versie 5.0.0
+-- truncate app_re cascade;
+-- truncate app_re_archief cascade;
+-- truncate app_re_kad_perceel cascade;
+-- truncate app_re_kad_perceel_archief cascade;
+-- truncate kad_onrrnd_zk cascade;
+-- truncate kad_onrrnd_zk_aantek cascade;
+-- truncate kad_onrrnd_zk_aantek_archief cascade;
+-- truncate kad_onrrnd_zk_archief cascade;
+-- truncate kad_onrrnd_zk_his_rel cascade;
+-- truncate kad_onrrnd_zk_kad_onrr_archief cascade;
+-- truncate kad_onrrnd_zk_kad_onrrnd_zk cascade;
+-- truncate kad_perceel cascade;
+-- truncate kad_perceel_archief cascade;
+-- truncate zak_recht_archief cascade;
+-- truncate zak_recht cascade;
+-- truncate zak_recht_aantek cascade;
+-- truncate zak_recht_archief cascade;
 
 --
 -- personen/subjecten ed. die mogelijk uit kadaster komen, mogelijk 
---    uit andere BR met personen (NHR, BRP)
+--    uit andere BR met personen (NHR, BRP, WOZ)
 -- controleer eerst met: 
 --        select distinct herkomst_br from herkomst_metadata;
--- als daar meer dan 1 type BR uit komt kunne tabellen niet met 
--- truncate worden geleegd omdat dan er meer dan BRK informatie verloren gaat
+-- als daar meer dan 1 type BR uit komt kunnen tabellen niet met
+-- truncate worden geleegd omdat dan er meer dan alleen BRK informatie verloren gaat
 --
---truncate ander_btnlnds_niet_nat_prs cascade;
---truncate ander_nat_prs cascade;
---truncate herkomst_metadata cascade;
---truncate ingeschr_nat_prs cascade;
---truncate ingeschr_niet_nat_prs cascade;
---truncate nat_prs cascade;
---truncate niet_ingezetene cascade;
---truncate niet_nat_prs cascade;
---truncate prs cascade;
---truncate subject cascade;
+truncate ander_btnlnds_niet_nat_prs cascade;
+truncate ander_nat_prs cascade;
+truncate herkomst_metadata cascade;
+truncate ingeschr_nat_prs cascade;
+truncate ingeschr_niet_nat_prs cascade;
+truncate nat_prs cascade;
+truncate niet_ingezetene cascade;
+truncate niet_nat_prs cascade;
+truncate prs cascade;
+truncate subject cascade;
