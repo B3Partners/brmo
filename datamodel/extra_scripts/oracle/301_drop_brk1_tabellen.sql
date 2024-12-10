585387ac90a9d8ffa52d7fc4555d66beaba8b420
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-12-10 14:08:42 +0100

    [BRMO-372] Opruimen van BRK 1 tabellen uit RSGB schema

--- datamodel/extra_scripts/oracle/301_drop_brk1_tabellen.sql
+++ datamodel/extra_scripts/oracle/301_drop_brk1_tabellen.sql
@@ -31,12 +31,49 @@ BEGIN
                                                  'VB_UTIL_APP_RE_PARENT',
                                                  'VB_UTIL_APP_RE_PARENT_2',
                                                  'VB_UTIL_APP_RE_PARENT_3',
-                                                 'VB_UTIL_APP_RE_SPLITSING',))
+                                                 'VB_UTIL_APP_RE_SPLITSING'))
         )
         LOOP
             BEGIN
                 EXECUTE IMMEDIATE 'DELETE FROM user_sdo_geom_metadata WHERE table_name =''' || i.column_value || '''';
                 EXECUTE IMMEDIATE 'DROP VIEW ' || i.column_value || ' CASCADE CONSTRAINTS';
+            EXCEPTION
+                WHEN OTHERS THEN
+                    IF SQLCODE != -942 THEN
+                        RAISE;
+                    END IF;
+            END;
+        END LOOP;
+END;
+/
+
+BEGIN
+    FOR i IN (SELECT column_value
+              FROM table (SYS.DBMS_DEBUG_VC2COLL('KAD_PERCEEL_ARCHIEF',
+                                                 'KAD_ONRRND_ZK_KAD_ONRR_ARCHIEF',
+                                                 'APP_RE_KAD_PERCEEL_ARCHIEF',
+                                                 'APP_RE_ARCHIEF',
+                                                 'ZAK_RECHT_ARCHIEF',
+                                                 'ZAK_RECHT_AANTEK',
+                                                 'KAD_ONRRND_ZK_KAD_ONRRND_ZK',
+                                                 'KAD_ONRRND_ZK_AANTEK',
+                                                 'APP_RE_KAD_PERCEEL',
+                                                 'KAD_ONRRND_ZK_HIS_REL',
+                                                 'KAD_ONRRND_ZK_ARCHIEF',
+                                                 'KAD_ONRRND_ZK_AANTEK_ARCHIEF',
+                                                 'BENOEMD_OBJ_KAD_ONRRND_ZK',
+                                                 'ZAK_RECHT',
+                                                 'APP_RE',
+                                                 'KAD_PERCEEL',
+                                                 'KAD_ONRRND_ZK',
+                                                 'KAD_GEMEENTE',
+                                                 'AARD_RECHT_VERKORT',
+                                                 'AARD_VERKREGEN_RECHT'))
+        )
+        LOOP
+            BEGIN
+                EXECUTE IMMEDIATE 'DELETE FROM user_sdo_geom_metadata WHERE table_name =''' || i.column_value || '''';
+                EXECUTE IMMEDIATE 'DROP TABLE ' || i.column_value || ' CASCADE CONSTRAINTS PURGE';
             EXCEPTION
                 WHEN OTHERS THEN
                     IF SQLCODE != -942 THEN
--- datamodel/extra_scripts/postgresql/301_drop_brk1_tabellen.sql
+++ datamodel/extra_scripts/postgresql/301_drop_brk1_tabellen.sql
@@ -16,3 +16,25 @@ drop view if exists vb_util_app_re_parent_3 cascade;
 drop view if exists vb_util_app_re_splitsing cascade;
 drop materialized view if exists mb_avg_subject cascade;
 drop materialized view if exists mb_subject cascade;
+
+-- verwijderen van BRK 1 gerelateerde tabellen
+drop table if exists kad_perceel_archief cascade;
+drop table if exists kad_onrrnd_zk_kad_onrr_archief cascade;
+drop table if exists app_re_kad_perceel_archief cascade;
+drop table if exists app_re_archief cascade;
+drop table if exists zak_recht_archief cascade;
+drop table if exists zak_recht_aantek cascade;
+drop table if exists kad_onrrnd_zk_kad_onrrnd_zk cascade;
+drop table if exists kad_onrrnd_zk_aantek cascade;
+drop table if exists app_re_kad_perceel cascade;
+drop table if exists kad_onrrnd_zk_his_rel cascade;
+drop table if exists kad_onrrnd_zk_archief cascade;
+drop table if exists kad_onrrnd_zk_aantek_archief cascade;
+drop table if exists benoemd_obj_kad_onrrnd_zk cascade;
+drop table if exists zak_recht cascade;
+drop table if exists app_re cascade;
+drop table if exists kad_perceel cascade;
+drop table if exists kad_onrrnd_zk cascade;
+drop table if exists kad_gemeente cascade;
+drop table if exists aard_recht_verkort cascade;
+drop table if exists aard_verkregen_recht cascade;
