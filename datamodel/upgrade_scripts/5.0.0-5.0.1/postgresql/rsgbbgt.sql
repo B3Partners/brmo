61c71ca79df3a59814bd85417eebb2e66c34f2ac
Mark Prins <1165786+mprins@users.noreply.github.com>
2024-12-27 12:45:47 +0100

    Migratie bestanden aanmaken voor upgrade 5.0.0-5.0.1

---
+++ datamodel/upgrade_scripts/5.0.0-5.0.1/postgresql/rsgbbgt.sql
@@ -0,0 +1,12 @@
+-- 
+-- upgrade PostgreSQL RSGBBGT datamodel van 5.0.0 naar 5.0.1
+--
+
+CREATE TABLE IF NOT EXISTS brmo_metadata(naam CHARACTER VARYING(255) NOT NULL, waarde TEXT, CONSTRAINT brmo_metadata_pk PRIMARY KEY (naam));
+INSERT INTO brmo_metadata(naam) VALUES('brmoversie') ON CONFLICT DO NOTHING;
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_5.0.0_naar_5.0.1','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='5.0.0' WHERE naam='brmoversie';
