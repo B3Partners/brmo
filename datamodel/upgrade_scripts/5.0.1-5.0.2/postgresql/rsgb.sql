0f24fb7c82294070cdd3fe8b6514a688937248d7
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-02-27 12:52:15 +0100

    Migratie bestanden aanmaken voor upgrade 5.0.1-5.0.2

---
+++ datamodel/upgrade_scripts/5.0.1-5.0.2/postgresql/rsgb.sql
@@ -0,0 +1,11 @@
+-- 
+-- upgrade PostgreSQL RSGB datamodel van 5.0.1 naar 5.0.2 
+--
+
+set search_path = public,bag,brk;
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_5.0.1_naar_5.0.2','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='5.0.2' WHERE naam='brmoversie';
