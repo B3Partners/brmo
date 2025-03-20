8346c049536a10b4b7ae6b4ae212d115a53d09b3
Matthijs Laan <matthijsln@gmail.com>
2025-03-20 14:51:52 +0100

    Update script

--- datamodel/extra_scripts/oracle/117_versienummer.sql
+++ datamodel/extra_scripts/oracle/117_versienummer.sql
@@ -3,7 +3,7 @@
 CREATE TABLE BRMO_METADATA
     (
         NAAM VARCHAR2(255 CHAR) NOT NULL,
-        WAARDE VARCHAR2(255 CHAR),
+        WAARDE CLOB,
         PRIMARY KEY (NAAM)
     );
 COMMENT ON TABLE BRMO_METADATA IS 'BRMO systeem metadata en versie gegevens';
--- datamodel/extra_scripts/postgresql/117_versienummer.sql
+++ datamodel/extra_scripts/postgresql/117_versienummer.sql
@@ -3,7 +3,7 @@
 CREATE TABLE brmo_metadata
     (
         naam CHARACTER VARYING(255) NOT NULL,
-        waarde CHARACTER VARYING(255),
+        waarde TEXT,
         CONSTRAINT brmo_metadata_pk PRIMARY KEY (naam)
     );
 COMMENT ON TABLE brmo_metadata IS 'BRMO systeem metadata en versie gegevens';
