27f20a10944403f0d832c4032dea449fe7879270
Matthijs <matthijsln@gmail.com>
2025-03-20 15:23:33 +0100

    BRMO-143: Change brmo_metadata.waarde column type to clob/text (#2401)

--- datamodel/upgrade_scripts/5.0.1-5.0.2/oracle/rsgb.sql
+++ datamodel/upgrade_scripts/5.0.1-5.0.2/oracle/rsgb.sql
@@ -4,6 +4,10 @@
 
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 
+alter table brmo_metadata add (waarde_clob clob);
+update brmo_metadata set waarde_clob = waarde;
+alter table brmo_metadata drop column waarde;
+alter table brmo_metadata rename column waarde_clob to waarde;
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_5.0.1_naar_5.0.2','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
--- datamodel/upgrade_scripts/5.0.1-5.0.2/postgresql/rsgb.sql
+++ datamodel/upgrade_scripts/5.0.1-5.0.2/postgresql/rsgb.sql
@@ -4,6 +4,7 @@
 
 set search_path = public,bag,brk;
 
+alter table brmo_metadata alter column waarde type text;
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_5.0.1_naar_5.0.2','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
