9de781e6f440aec788ab4269177666609442735b
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-05-19 10:55:36 +0200

    Migratie bestanden aanmaken voor upgrade 5.0.2-5.0.3

---
+++ datamodel/upgrade_scripts/5.0.2-5.0.3/oracle/rsgb.sql
@@ -0,0 +1,11 @@
+-- 
+-- upgrade Oracle RSGB datamodel van 5.0.2 naar 5.0.3 
+--
+
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+
+
+-- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
+INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_5.0.2_naar_5.0.3','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
+-- versienummer update
+UPDATE brmo_metadata SET waarde='5.0.3' WHERE naam='brmoversie';
