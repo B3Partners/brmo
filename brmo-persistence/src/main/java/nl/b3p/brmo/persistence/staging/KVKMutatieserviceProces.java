0eef14d8661fa2ea9240834bed3ce784647762f9
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-07-24 15:28:30 +0200

    [BRMO-404] Formulier voor automatische processen pagina

--- brmo-persistence/src/main/java/nl/b3p/brmo/persistence/staging/KVKMutatieserviceProces.java
+++ brmo-persistence/src/main/java/nl/b3p/brmo/persistence/staging/KVKMutatieserviceProces.java
@@ -3,9 +3,10 @@ package nl.b3p.brmo.persistence.staging;
 import javax.persistence.Entity;
 
 /**
- * KVKMutatieServiceProces is an entity representing a process for accessing the KVK Mutatie Service.
+ * KVKMutatieServiceProces is an entity representing a process for accessing the KVK Mutatie
+ * Service.
  *
  * @author mprins
  */
 @Entity
-public class KVKMutatieserviceProces extends AutomatischProces{}
+public class KVKMutatieserviceProces extends AutomatischProces {}
