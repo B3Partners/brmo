0b16602441c3ebe50d0ff1c5b96a719b8f1a0679
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-07-31 11:25:12 +0200

    opruimen van niet langer beschikbaar LaadprocesTransformatieProces

--- datamodel/upgrade_scripts/5.0.2-5.0.3/postgresql/staging.sql
+++ datamodel/upgrade_scripts/5.0.2-5.0.3/postgresql/staging.sql
@@ -2,6 +2,12 @@
 -- upgrade PostgreSQL STAGING datamodel van 5.0.2 naar 5.0.3 
 --
 
+-- opruimen van niet langer beschikbare automatische scanner processen
+DELETE FROM automatisch_proces_config
+USING automatisch_proces
+WHERE automatisch_proces_config.proces_id = automatisch_proces.id
+  AND automatisch_proces.dtype IN ('LaadprocesTransformatieProces');
+DELETE FROM automatisch_proces WHERE dtype IN ('LaadprocesTransformatieProces');
 
 -- onderstaande dienen als laatste stappen van een upgrade uitgevoerd
 INSERT INTO brmo_metadata (naam,waarde) SELECT 'upgrade_5.0.2_naar_5.0.3','vorige versie was ' || waarde FROM brmo_metadata WHERE naam='brmoversie';
