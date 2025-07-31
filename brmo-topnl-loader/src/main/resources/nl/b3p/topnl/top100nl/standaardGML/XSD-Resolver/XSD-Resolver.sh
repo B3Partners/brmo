98b5a52cf92ad21d6b12c5198e841af16af6e622
Mark Prins <1165786+mprins@users.noreply.github.com>
2025-07-31 17:58:49 +0200

    verwijder brmo-topnl-loader module

--- brmo-topnl-loader/src/main/resources/nl/b3p/topnl/top100nl/standaardGML/XSD-Resolver/XSD-Resolver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./remove.sh
-java -Xmx1024m -jar saxon/saxon9he.jar -o:generated/temp.xml -s:input/configuration.xml -xsl:xslt/resolveXSDs.xslt versie=
--- brmo-topnl-loader/src/main/resources/nl/b3p/topnl/top250nl/standaardGML/XSD-Resolver/finalized/convertToJava.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -Rf ../../../../../../../../java/nl/b3p/topnl/top250nl
-xjc -p nl.b3p.topnl.top250nl -b bindings.xml -d ../../../../../../../../java/ top250nl_resolved.xsd
